44e9daf8fcb1f6ca583f30b6c9e8d787be7e8a7a
Kotori316 <Kotori316@gmail.com>
2017-09-30 09:13:28 +0900

    Small fix

--- src/main/scala/com/yogpc/qp/render/Sprites.scala
+++ src/main/scala/com/yogpc/qp/render/Sprites.scala
@@ -18,10 +18,10 @@ object Sprites {
     @SubscribeEvent
     def registerTexture(event: TextureStitchEvent.Pre): Unit = {
         LaserType.values().foreach(laserType => map.put(laserType.symbol, event.getMap.registerSprite(laserType.location())))
-        //        map.put('laser_1, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_1")))
-        //        map.put('laser_2, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_2")))
-        //        map.put('laser_3, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_3")))
-        //        map.put('laser_4, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_4")))
+        map.put('laser_1, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_1")))
+        map.put('laser_2, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_2")))
+        map.put('laser_3, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_3")))
+        map.put('laser_4, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/laser_4")))
         //        map.put('stripes, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/stripes")))
         map.put('stripes_h, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/stripes_h")))
         map.put('stripes_v, event.getMap.registerSprite(new ResourceLocation(QuarryPlus.modID, "entities/stripes_v")))
