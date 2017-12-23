56174dd0e0bcfdcb8faa8592974e7909eb6d6995
Kotori316 <Kotori316@gmail.com>
2017-12-23 13:35:09 +0900

    Dummy commit.

--- src/main/scala/com/yogpc/qp/tile/APowerTile.java
+++ src/main/scala/com/yogpc/qp/tile/APowerTile.java
@@ -44,7 +44,7 @@ import buildcraft.api.tiles.IDebuggable;*/
         @Optional.Interface(iface = "cofh.api.tileentity.IEnergyInfo", modid = QuarryPlus.Optionals.COFH_tileentity),
         @Optional.Interface(iface = "buildcraft.api.tiles.IDebuggable", modid = QuarryPlus.Optionals.Buildcraft_tiles),
         @Optional.Interface(iface = "ic2.api.energy.tile.IEnergySink", modid = QuarryPlus.Optionals.IC2_modID)})
-public abstract class APowerTile extends APacketTile implements IEnergyReceiver, IEnergySink, ITickable, IEnergyStorage, IEnergyInfo/*, IDebuggable*/ {
+public abstract class APowerTile extends APacketTile implements ITickable, IEnergyStorage, IEnergyReceiver, IEnergyInfo, IEnergySink/*, IDebuggable*/ {
     /*package-private*/ double all, maxGot, max, got;
     private boolean ic2ok = false;
     public boolean bcLoaded;
