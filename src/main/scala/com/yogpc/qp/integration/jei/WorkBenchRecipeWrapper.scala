6ab089914aca2e75cbf038b8cea01aed42e50da7
Kotori316 <Kotori316@gmail.com>
2018-09-23 16:48:38 +0900

    In Jei recipe, progress bar is animating and improved.

--- src/main/scala/com/yogpc/qp/integration/jei/WorkBenchRecipeWrapper.scala
+++ src/main/scala/com/yogpc/qp/integration/jei/WorkBenchRecipeWrapper.scala
@@ -5,6 +5,7 @@ import java.util.{Collections, ArrayList => AList, List => JList}
 import com.yogpc.qp.tile.WorkbenchRecipes
 import mezz.jei.api.ingredients.IIngredients
 import mezz.jei.api.recipe.IRecipeWrapper
+import net.minecraft.client.Minecraft
 import net.minecraft.item.ItemStack
 
 import scala.collection.JavaConverters._
@@ -19,6 +20,11 @@ class WorkBenchRecipeWrapper(recipe: WorkbenchRecipes) extends IRecipeWrapper wi
 
         ingredients.setInputLists(classOf[ItemStack], inputs)
         ingredients.setOutputs(classOf[ItemStack], outputs)
+    }
+
+    override def drawInfo(minecraft: Minecraft, recipeWidth: Int, recipeHeight: Int, mouseX: Int, mouseY: Int): Unit = {
+        import WorkBenchRecipeCategory._
+        minecraft.fontRenderer.drawString(getEnergyRequired.toString + "MJ", 40 - xOff, 74 - yOff, 0x404040)
     }
 
     val getEnergyRequired: Double = recipe.energy
