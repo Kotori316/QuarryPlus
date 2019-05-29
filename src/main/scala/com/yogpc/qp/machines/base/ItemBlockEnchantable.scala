c32f966376fdd86950118a3afdc1ac358af457fc
Kotori316 <Kotori316@gmail.com>
2019-05-29 11:43:19 +0900

    Removed limit of enchantment

--- src/main/scala/com/yogpc/qp/machines/base/ItemBlockEnchantable.scala
+++ src/main/scala/com/yogpc/qp/machines/base/ItemBlockEnchantable.scala
@@ -2,6 +2,8 @@ package com.yogpc.qp.machines.base
 
 import java.util.function.Predicate
 
+import com.yogpc.qp.Config
+import com.yogpc.qp.machines.bookmover.BlockBookMover
 import net.minecraft.block.Block
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.item.{BlockItemUseContext, Item, ItemBlock, ItemStack}
@@ -10,11 +12,14 @@ abstract class ItemBlockEnchantable(b: Block, prop: Item.Properties) extends Ite
 
   def tester(is: ItemStack): Predicate[Enchantment]
 
-  override def canMove(is: ItemStack, enchantment: Enchantment) = tester(is).test(enchantment)
+  override def canMove(is: ItemStack, enchantment: Enchantment) = {
+    val stack = if (Config.common.disabled(BlockBookMover.SYMBOL).get()) is else new ItemStack(is.getItem.asItem(), is.getCount)
+    tester(stack).test(enchantment)
+  }
 
   override def isBookEnchantable(s1: ItemStack, s2: ItemStack) = false
 
-  override def tryPlace(context : BlockItemUseContext) = {
+  override def tryPlace(context: BlockItemUseContext) = {
     if (Option(context.getPlayer).exists(_.abilities.isCreativeMode)) {
       val size = context.getItem.getCount
       val result = super.tryPlace(context)
