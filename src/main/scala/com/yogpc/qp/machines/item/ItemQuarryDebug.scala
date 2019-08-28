3e6183d91c05b53e622103872ad5fc144256fefa
Kotori316 <Kotori316@gmail.com>
2019-08-29 00:57:44 +0900

    Debugger for 1.14.4

--- src/main/scala/com/yogpc/qp/machines/item/ItemQuarryDebug.scala
+++ src/main/scala/com/yogpc/qp/machines/item/ItemQuarryDebug.scala
@@ -5,8 +5,8 @@ import com.yogpc.qp.utils.Holder
 import com.yogpc.qp.{Config, QuarryPlus}
 import net.minecraft.item.{Item, ItemGroup, ItemStack, ItemUseContext}
 import net.minecraft.tileentity.TileEntity
-import net.minecraft.util.text.{ITextComponent, TextComponentString, TextComponentTranslation}
-import net.minecraft.util.{EnumActionResult, NonNullList}
+import net.minecraft.util.text.{ITextComponent, StringTextComponent, TranslationTextComponent}
+import net.minecraft.util.{ActionResultType, NonNullList}
 
 class ItemQuarryDebug extends Item((new Item.Properties).group(Holder.tab)) {
 
@@ -14,13 +14,13 @@ class ItemQuarryDebug extends Item((new Item.Properties).group(Holder.tab)) {
 
   setRegistryName(QuarryPlus.modID, QuarryPlus.Names.debug)
 
-  override def onItemUseFirst(stack: ItemStack, context: ItemUseContext): EnumActionResult = {
+  override def onItemUseFirst(stack: ItemStack, context: ItemUseContext): ActionResultType = {
     val worldIn = context.getWorld
     val pos = context.getPos
     val player = context.getPlayer
 
     if (worldIn.isRemote) {
-      EnumActionResult.PASS
+      ActionResultType.PASS
     } else {
       if (Config.common.debug) {
         val tile = worldIn.getTileEntity(pos)
@@ -30,29 +30,29 @@ class ItemQuarryDebug extends Item((new Item.Properties).group(Holder.tab)) {
               if (player.isSneaking) {
                 plusMachine.toggleOutputEnergyInfo()
               } else {
-                player.sendStatusMessage(new TextComponentTranslation(plusMachine.getDebugName), false)
+                player.sendStatusMessage(new TranslationTextComponent(plusMachine.getDebugName), false)
                 player.sendStatusMessage(tilePosToString(tile), false)
                 player.sendStatusMessage(energyToString(plusMachine), false)
                 plusMachine.sendDebugMessage(player)
               }
             }
-            EnumActionResult.SUCCESS
+            ActionResultType.SUCCESS
           case sender: IDebugSender =>
             if (!worldIn.isRemote) {
-              player.sendStatusMessage(new TextComponentTranslation(sender.getDebugName), false)
+              player.sendStatusMessage(new TranslationTextComponent(sender.getDebugName), false)
               player.sendStatusMessage(tilePosToString(sender), false)
               sender.sendDebugMessage(player)
             }
-            EnumActionResult.SUCCESS
+            ActionResultType.SUCCESS
           /*case placer: TilePlacer =>
             if (!worldIn.isRemote) {
-              player.sendStatusMessage(new TextComponentTranslation(placer.getName), false)
+              player.sendStatusMessage(new TranslationTextComponent(placer.getName), false)
               player.sendStatusMessage(tilePosToString(tile), false)
             }
             EnumActionResult.SUCCESS
           case pump: TilePump =>
             if (!worldIn.isRemote) {
-              player.sendStatusMessage(new TextComponentTranslation(pump.getDebugName), false)
+              player.sendStatusMessage(new TranslationTextComponent(pump.getDebugName), false)
               player.sendStatusMessage(tilePosToString(tile), false)
               pump.sendDebugMessage(player)
             }
@@ -60,7 +60,7 @@ class ItemQuarryDebug extends Item((new Item.Properties).group(Holder.tab)) {
           case _ => super.onItemUseFirst(stack, context)
         }
       } else {
-        player.sendStatusMessage(new TextComponentString("QuarryPlus debug is not enabled"), true)
+        player.sendStatusMessage(new StringTextComponent("QuarryPlus debug is not enabled"), true)
         super.onItemUseFirst(stack, context)
       }
     }
@@ -75,11 +75,11 @@ class ItemQuarryDebug extends Item((new Item.Properties).group(Holder.tab)) {
 object ItemQuarryDebug {
 
   def tilePosToString(tile: TileEntity): ITextComponent = {
-    new TextComponentString(s"Tile Pos : x=${tile.getPos.getX}, y=${tile.getPos.getY}, z=${tile.getPos.getZ}")
+    new StringTextComponent(s"Tile Pos : x=${tile.getPos.getX}, y=${tile.getPos.getY}, z=${tile.getPos.getZ}")
   }
 
   def energyToString(tile: APowerTile): ITextComponent = {
-    if (Config.common.noEnergy.get()) new TextComponentString("No Energy Required.")
-    else new TextComponentString(tile.getStoredEnergy / APowerTile.MicroJtoMJ + " / " + tile.getMaxStored / APowerTile.MicroJtoMJ + " MJ")
+    if (Config.common.noEnergy.get()) new StringTextComponent("No Energy Required.")
+    else new StringTextComponent(tile.getStoredEnergy / APowerTile.MicroJtoMJ + " / " + tile.getMaxStored / APowerTile.MicroJtoMJ + " MJ")
   }
 }
