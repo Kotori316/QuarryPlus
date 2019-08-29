2136a600abcb4654774d5b3e1aeb64599459acaf
Kotori316 <Kotori316@gmail.com>
2019-08-29 12:29:06 +0900

    util

--- src/main/scala/com/yogpc/qp/package.scala
+++ src/main/scala/com/yogpc/qp/package.scala
@@ -7,8 +7,8 @@ import net.minecraft.enchantment.Enchantment
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt._
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.text.TextComponentString
-import net.minecraft.util.{EnumFacing, ResourceLocation}
+import net.minecraft.util.text.StringTextComponent
+import net.minecraft.util.{Direction, ResourceLocation}
 import net.minecraftforge.common.util.Constants.NBT
 import net.minecraftforge.common.util.{INBTSerializable, LazyOptional, NonNullSupplier}
 import net.minecraftforge.fluids.FluidStack
@@ -16,15 +16,15 @@ import net.minecraftforge.registries.ForgeRegistries
 
 package object qp {
 
-  type NBTWrapper[A, NBTType <: INBTBase] = A => NBTType
+  type NBTWrapper[A, NBTType <: INBT] = A => NBTType
   type Cap[T] = OptionT[Eval, T]
 
   val enchantCollector: PartialFunction[(ResourceLocation, Int), (ResourceLocation, Integer)] = {
     case (a, b) if b > 0 => (a, Int.box(b))
   }
-  val toComponentString: String => TextComponentString = s => new TextComponentString(s)
+  val toComponentString: String => StringTextComponent = s => new StringTextComponent(s)
   val nonNull: AnyRef => Boolean = obj => obj != null
-  val facings = Eval.later(EnumFacing.values()).map(_.toList)
+  val facings = Eval.later(Direction.values()).map(_.toList)
 
   def toJavaOption[T](o: Option[T]): java.util.Optional[T] = {
     //I think it's faster than match function.
@@ -58,14 +58,14 @@ package object qp {
   implicit class ItemStackRemoveEnchantment(val stack: ItemStack) extends AnyVal {
     def removeEnchantment(enchantment: Enchantment): Unit = {
       val id = ForgeRegistries.ENCHANTMENTS.getKey(enchantment)
-      val tagName = if (stack.getItem == net.minecraft.init.Items.ENCHANTED_BOOK) "StoredEnchantments" else "ench"
-      val list = Option(stack.getTag).fold(new NBTTagList)(_.getList(tagName, NBT.TAG_COMPOUND))
+      val tagName = if (stack.getItem == net.minecraft.item.Items.ENCHANTED_BOOK) "StoredEnchantments" else "ench"
+      val list = Option(stack.getTag).fold(new ListNBT)(_.getList(tagName, NBT.TAG_COMPOUND))
 
       import scala.collection.JavaConverters._
-      val copied = list.asScala.zipWithIndex.map { case (t, i) => (t.asInstanceOf[NBTTagCompound], i) }
+      val copied = list.asScala.zipWithIndex.map { case (t, i) => (t.asInstanceOf[CompoundNBT], i) }
       for ((tag, i) <- copied) {
         if (tag.getString("id") == id.toString) {
-          list.removeTag(i)
+          list.remove(i)
         }
       }
       Option(stack.getTag).foreach(subtag => {
@@ -83,19 +83,19 @@ package object qp {
     }
   }
 
-  implicit val Long2NBT: NBTWrapper[Long, NBTTagLong] = (num: Long) => new NBTTagLong(num)
-  implicit val int2NBT: NBTWrapper[Int, NBTTagInt] = (num: Int) => new NBTTagInt(num)
-  implicit val bool2NBT: NBTWrapper[Boolean, NBTTagByte] = (b: Boolean) => new NBTTagByte(if (b) 1 else 0)
+  implicit val Long2NBT: NBTWrapper[Long, LongNBT] = (num: Long) => new LongNBT(num)
+  implicit val int2NBT: NBTWrapper[Int, IntNBT] = (num: Int) => new IntNBT(num)
+  implicit val bool2NBT: NBTWrapper[Boolean, ByteNBT] = (b: Boolean) => new ByteNBT(if (b) 1 else 0)
 
-  implicit val Fluid2NBT: NBTWrapper[FluidStack, NBTTagCompound] = (num: FluidStack) => num.writeToNBT(new NBTTagCompound)
-  implicit val NBTSerializable2NBT: INBTSerializable[NBTTagCompound] NBTWrapper NBTTagCompound = _.serializeNBT()
+  implicit val Fluid2NBT: NBTWrapper[FluidStack, CompoundNBT] = (num: FluidStack) => num.writeToNBT(new CompoundNBT)
+  implicit val NBTSerializable2NBT: INBTSerializable[CompoundNBT] NBTWrapper CompoundNBT = _.serializeNBT()
 
   implicit class NumberToNbt[A](private val num: A) extends AnyVal {
-    def toNBT[B <: INBTBase](implicit wrapper: NBTWrapper[A, B]): B = wrapper apply num
+    def toNBT[B <: INBT](implicit wrapper: NBTWrapper[A, B]): B = wrapper apply num
   }
 
   implicit class PosHelper(val blockPos: BlockPos) extends AnyVal {
-    def offset(facing1: EnumFacing, facing2: EnumFacing): BlockPos = {
+    def offset(facing1: Direction, facing2: Direction): BlockPos = {
       val x = facing1.getXOffset + facing2.getXOffset
       val y = facing1.getYOffset + facing2.getYOffset
       val z = facing1.getZOffset + facing2.getZOffset
