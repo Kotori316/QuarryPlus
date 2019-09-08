a5feeeee941b0261da52c563f9f55c757df0fede
Kotori316 <Kotori316@gmail.com>
2019-09-09 00:18:04 +0900

    You can get y level in tile's inventory GUI.

--- src/main/scala/com/yogpc/qp/gui/GuiQuarryModule.java
+++ src/main/scala/com/yogpc/qp/gui/GuiQuarryModule.java
@@ -1,7 +1,12 @@
 package com.yogpc.qp.gui;
 
+import java.util.function.IntSupplier;
+
 import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.container.ContainerQuarryModule;
+import com.yogpc.qp.tile.TileAdvQuarry;
+import com.yogpc.qp.tile.TileQuarry2;
+import javax.annotation.Nullable;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
@@ -12,10 +17,24 @@ public class GuiQuarryModule extends GuiContainer {
 
     private static final ResourceLocation LOCATION = new ResourceLocation(QuarryPlus.modID, "textures/gui/quarry_module.png");
     private final String name;
+    @Nullable
+    private final IntSupplier yGetter;
 
     public GuiQuarryModule(ContainerQuarryModule.HasModuleInventory quarry, EntityPlayer player, String name) {
         super(new ContainerQuarryModule(quarry, player));
         this.name = name;
+
+        IntSupplier a;
+        if (quarry instanceof TileQuarry2) {
+            TileQuarry2 quarry2 = (TileQuarry2) quarry;
+            a = () -> GuiQuarryLevel.NQuarryY().getYLevel(quarry2);
+        } else if (quarry instanceof TileAdvQuarry) {
+            TileAdvQuarry advQuarry = (TileAdvQuarry) quarry;
+            a = () -> GuiQuarryLevel.AdvY().getYLevel(advQuarry);
+        } else {
+            a = null;
+        }
+        this.yGetter = a;
     }
 
     @Override
@@ -35,6 +54,8 @@ public class GuiQuarryModule extends GuiContainer {
     @Override
     protected void drawGuiContainerForegroundLayer(final int mouseX, final int mouseY) {
         this.fontRenderer.drawString(I18n.format(name), 8, 6, 0x404040);
+        if (yGetter != null)
+            this.fontRenderer.drawString("Y: " + yGetter.getAsInt(), 120, 6, 0x404040);
         this.fontRenderer.drawString(I18n.format(TranslationKeys.CONTAINER_INVENTORY), 8, this.ySize - 96 + 2, 0x404040);
     }
 }
