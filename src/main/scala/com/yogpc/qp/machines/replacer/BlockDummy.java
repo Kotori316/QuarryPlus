d93808a49049a14ce15b20229faa82cd7ede725b
Kotori316 <Kotori316@gmail.com>
2020-01-09 20:55:38 +0900

    Transparent dummy block

--- src/main/scala/com/yogpc/qp/machines/replacer/BlockDummy.java
+++ src/main/scala/com/yogpc/qp/machines/replacer/BlockDummy.java
@@ -2,24 +2,30 @@ package com.yogpc.qp.machines.replacer;
 
 import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.utils.Holder;
+import javax.annotation.Nullable;
+import net.minecraft.block.AbstractGlassBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntitySpawnPlacementRegistry;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockReader;
 
-public class BlockDummy extends Block {
+public class BlockDummy extends AbstractGlassBlock {
     private final BlockItem mItemBlock;
 
     public BlockDummy() {
         super(Block.Properties.create(Material.GLASS)
             .hardnessAndResistance(1f)
             .lightValue(15)
-            .noDrops());
+            .noDrops()
+            .sound(SoundType.GLASS)
+            .func_226896_b_());
         setRegistryName(QuarryPlus.modID, QuarryPlus.Names.dummyblock);
         mItemBlock = new BlockItem(this, new Item.Properties().group(Holder.tab()));
         mItemBlock.setRegistryName(QuarryPlus.modID, QuarryPlus.Names.dummyblock);
@@ -31,24 +37,12 @@ public class BlockDummy extends Block {
     }
 
     @Override
-    public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
-        return true;
-    }
-
-    @Override
     public boolean canDropFromExplosion(BlockState state, IBlockReader world, BlockPos pos, Explosion explosion) {
         return false;
     }
 
     @Override
-    @SuppressWarnings("deprecation")
-    public boolean isNormalCube(BlockState p_220081_1_, IBlockReader p_220081_2_, BlockPos p_220081_3_) {
-        return false;
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public boolean isSideInvisible(BlockState state, BlockState neighbor, Direction d) {
+    public boolean canCreatureSpawn(BlockState state, IBlockReader world, BlockPos pos, EntitySpawnPlacementRegistry.PlacementType type, @Nullable EntityType<?> entityType) {
         return false;
     }
 }
