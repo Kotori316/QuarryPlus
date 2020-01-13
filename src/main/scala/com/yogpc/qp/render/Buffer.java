ed0081c582723d2fc5599e22e4e241325004267d
Kotori316 <Kotori316@gmail.com>
2020-01-13 15:42:51 +0900

    Refactored to remove unused local variable.

--- src/main/scala/com/yogpc/qp/render/Buffer.java
+++ src/main/scala/com/yogpc/qp/render/Buffer.java
@@ -3,14 +3,10 @@ package com.yogpc.qp.render;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.Matrix4f;
-import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.Vector4f;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.Vec3i;
 
 final class Buffer {
     private final IVertexBuilder bufferBuilder;
-    private final Vector3f vector3f = new Vector3f();
     private final Vector4f vector4f = new Vector4f();
 
     Buffer(IVertexBuilder bufferBuilder) {
@@ -18,10 +14,7 @@ final class Buffer {
     }
 
     final Buffer pos(double x, double y, double z, MatrixStack matrix) {
-        Vec3i vec3i = Direction.UP.getDirectionVec();
-        vector3f.set(vec3i.getX(), vec3i.getY(), vec3i.getZ());
         Matrix4f matrix4f = matrix.func_227866_c_().func_227870_a_();
-        vector3f.func_229188_a_(matrix.func_227866_c_().func_227872_b_());
 
         vector4f.set((float) x, (float) y, (float) z, 1.0F);
         vector4f.func_229372_a_(matrix4f);
@@ -35,10 +28,10 @@ final class Buffer {
      * @return this
      */
     final Buffer colored() {
-        bufferBuilder.func_225586_a_(255, 255, 255, 255);
-        return this;
+        return this.color(255, 255, 255, 255);
     }
 
+    @SuppressWarnings("SameParameterValue")
     final Buffer color(int red, int green, int blue, int alpha) {
         bufferBuilder.func_225586_a_(red, green, blue, alpha);
         return this;
@@ -53,7 +46,7 @@ final class Buffer {
      * {@code buffer.lightmap(240, 0).endVertex()}
      */
     final void lightedAndEnd() {
-        bufferBuilder.func_225585_a_(10, 10).func_225587_b_(240, 0).func_225584_a_(0, 0, 0).endVertex();
+        bufferBuilder.func_225585_a_(10, 10).func_225587_b_(240, 0).func_225584_a_(0, 1, 0).endVertex();
     }
 
     final void lightedAndEnd(Box.LightValue value) {
