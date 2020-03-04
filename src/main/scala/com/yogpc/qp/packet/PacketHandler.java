bacac20cbb0a4417e1cb5ef6110f784173c22668
Kotori316 <Kotori316@gmail.com>
2020-03-05 00:52:27 +0900

    use int counter.

--- src/main/scala/com/yogpc/qp/packet/PacketHandler.java
+++ src/main/scala/com/yogpc/qp/packet/PacketHandler.java
@@ -1,5 +1,7 @@
 package com.yogpc.qp.packet;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.packet.advpump.AdvPumpChangeMessage;
 import com.yogpc.qp.packet.advpump.AdvPumpStatusMessage;
@@ -40,38 +42,36 @@ public class PacketHandler {
     );
 
     public static void init() {
-        int id = 1;
+        AtomicInteger id = new AtomicInteger(1);
 
-        INSTANCE.registerMessage(id++, LinkMessage.class, LinkMessage::writeToBuffer, IMessage.decode(LinkMessage::new), LinkMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, UpdateBoxMessage.class, UpdateBoxMessage::writeToBuffer, IMessage.decode(UpdateBoxMessage::new), UpdateBoxMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, TileMessage.class, TileMessage::writeToBuffer, IMessage.decode(TileMessage::new), TileMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, MoverMessage.Move.class, MoverMessage.Move::writeToBuffer, IMessage.decode(MoverMessage.Move::new), MoverMessage.Move::onReceiveInternal);
-        INSTANCE.registerMessage(id++, MoverMessage.Cursor.class, MoverMessage.Cursor::writeToBuffer, IMessage.decode(MoverMessage.Cursor::new), MoverMessage.Cursor::onReceiveInternal);
-        INSTANCE.registerMessage(id++, BlockListRequestMessage.class, BlockListRequestMessage::writeToBuffer, IMessage.decode(BlockListRequestMessage::new), BlockListRequestMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, DiffMessage.class, DiffMessage::writeToBuffer, IMessage.decode(DiffMessage::new), DiffMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, EnchantmentMessage.class, EnchantmentMessage::writeToBuffer, IMessage.decode(EnchantmentMessage::new), EnchantmentMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, LevelMessage.class, LevelMessage::writeToBuffer, IMessage.decode(LevelMessage::new), LevelMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, ModeMessage.class, ModeMessage::writeToBuffer, IMessage.decode(ModeMessage::new), ModeMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, MoveHead.class, MoveHead::writeToBuffer, IMessage.decode(MoveHead::new), MoveHead::onReceiveInternal);
-        INSTANCE.registerMessage(id++, Now.class, Now::writeToBuffer, IMessage.decode(Now::new), Now::onReceiveInternal);
-        INSTANCE.registerMessage(id++, Mappings.All.class, Mappings.All::writeToBuffer, IMessage.decode(Mappings.All::new), Mappings.All::onReceiveInternal);
-        INSTANCE.registerMessage(id++, Mappings.Copy.class, Mappings.Copy::writeToBuffer, IMessage.decode(Mappings.Copy::new), Mappings.Copy::onReceiveInternal);
-        INSTANCE.registerMessage(id++, Mappings.Update.class, Mappings.Update::writeToBuffer, IMessage.decode(Mappings.Update::new), Mappings.Update::onReceiveInternal);
-        INSTANCE.registerMessage(id++, SetEntity.class, SetEntity::writeToBuffer, IMessage.decode(SetEntity::new), SetEntity::onReceiveInternal);
-        INSTANCE.registerMessage(id++, AvailableEntities.class, AvailableEntities::writeToBuffer, IMessage.decode(AvailableEntities::new), AvailableEntities::onReceiveInternal);
-        INSTANCE.registerMessage(id++, AdvModeMessage.class, AdvModeMessage::writeToBuffer, IMessage.decode(AdvModeMessage::new), AdvModeMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, AdvActionMessage.class, AdvActionMessage::writeToBuffer, IMessage.decode(AdvActionMessage::new), AdvActionMessage::onReceiveInternal);
-//        INSTANCE.registerMessage(id++, AdvFilterMessage.class, AdvFilterMessage::writeToBuffer, IMessage.decode(AdvFilterMessage::new), AdvFilterMessage::onReceiveInternal);
-//        INSTANCE.registerMessage(id++, AdvContentMessage.class, AdvContentMessage::writeToBuffer, IMessage.decode(AdvContentMessage::new), AdvContentMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, AdvLevelMessage.class, AdvLevelMessage::writeToBuffer, b -> new AdvLevelMessage().readFromBuffer(b), AdvLevelMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, AdvPumpChangeMessage.class, AdvPumpChangeMessage::writeToBuffer, IMessage.decode(AdvPumpChangeMessage::new), AdvPumpChangeMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, AdvPumpStatusMessage.class, AdvPumpStatusMessage::writeToBuffer, IMessage.decode(AdvPumpStatusMessage::new), AdvPumpStatusMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, TemplateMessage.class, TemplateMessage::writeToBuffer, IMessage.decode(TemplateMessage::new), TemplateMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, ActionMessage.class, ActionMessage::writeToBuffer, IMessage.decode(ActionMessage::new), ActionMessage::onReceiveInternal);
-        INSTANCE.registerMessage(id++, Level2Message.class, Level2Message::writeToBuffer, b -> new Level2Message().readFromBuffer(b), Level2Message::onReceiveInternal);
-        INSTANCE.registerMessage(id++, RecipeSyncMessage.class, RecipeSyncMessage::writeToBuffer, IMessage.decode(RecipeSyncMessage::new), RecipeSyncMessage::onReceiveInternal);
-
-        assert id > 0;
+        INSTANCE.registerMessage(id.getAndIncrement(), LinkMessage.class, LinkMessage::writeToBuffer, IMessage.decode(LinkMessage::new), LinkMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), UpdateBoxMessage.class, UpdateBoxMessage::writeToBuffer, IMessage.decode(UpdateBoxMessage::new), UpdateBoxMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), TileMessage.class, TileMessage::writeToBuffer, IMessage.decode(TileMessage::new), TileMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), MoverMessage.Move.class, MoverMessage.Move::writeToBuffer, IMessage.decode(MoverMessage.Move::new), MoverMessage.Move::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), MoverMessage.Cursor.class, MoverMessage.Cursor::writeToBuffer, IMessage.decode(MoverMessage.Cursor::new), MoverMessage.Cursor::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), BlockListRequestMessage.class, BlockListRequestMessage::writeToBuffer, IMessage.decode(BlockListRequestMessage::new), BlockListRequestMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), DiffMessage.class, DiffMessage::writeToBuffer, IMessage.decode(DiffMessage::new), DiffMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), EnchantmentMessage.class, EnchantmentMessage::writeToBuffer, IMessage.decode(EnchantmentMessage::new), EnchantmentMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), LevelMessage.class, LevelMessage::writeToBuffer, IMessage.decode(LevelMessage::new), LevelMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), ModeMessage.class, ModeMessage::writeToBuffer, IMessage.decode(ModeMessage::new), ModeMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), MoveHead.class, MoveHead::writeToBuffer, IMessage.decode(MoveHead::new), MoveHead::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), Now.class, Now::writeToBuffer, IMessage.decode(Now::new), Now::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), Mappings.All.class, Mappings.All::writeToBuffer, IMessage.decode(Mappings.All::new), Mappings.All::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), Mappings.Copy.class, Mappings.Copy::writeToBuffer, IMessage.decode(Mappings.Copy::new), Mappings.Copy::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), Mappings.Update.class, Mappings.Update::writeToBuffer, IMessage.decode(Mappings.Update::new), Mappings.Update::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), SetEntity.class, SetEntity::writeToBuffer, IMessage.decode(SetEntity::new), SetEntity::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), AvailableEntities.class, AvailableEntities::writeToBuffer, IMessage.decode(AvailableEntities::new), AvailableEntities::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), AdvModeMessage.class, AdvModeMessage::writeToBuffer, IMessage.decode(AdvModeMessage::new), AdvModeMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), AdvActionMessage.class, AdvActionMessage::writeToBuffer, IMessage.decode(AdvActionMessage::new), AdvActionMessage::onReceiveInternal);
+//        INSTANCE.registerMessage(id.getAndIncrement(), AdvFilterMessage.class, AdvFilterMessage::writeToBuffer, IMessage.decode(AdvFilterMessage::new), AdvFilterMessage::onReceiveInternal);
+//        INSTANCE.registerMessage(id.getAndIncrement(), AdvContentMessage.class, AdvContentMessage::writeToBuffer, IMessage.decode(AdvContentMessage::new), AdvContentMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), AdvLevelMessage.class, AdvLevelMessage::writeToBuffer, b -> new AdvLevelMessage().readFromBuffer(b), AdvLevelMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), AdvPumpChangeMessage.class, AdvPumpChangeMessage::writeToBuffer, IMessage.decode(AdvPumpChangeMessage::new), AdvPumpChangeMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), AdvPumpStatusMessage.class, AdvPumpStatusMessage::writeToBuffer, IMessage.decode(AdvPumpStatusMessage::new), AdvPumpStatusMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), TemplateMessage.class, TemplateMessage::writeToBuffer, IMessage.decode(TemplateMessage::new), TemplateMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), ActionMessage.class, ActionMessage::writeToBuffer, IMessage.decode(ActionMessage::new), ActionMessage::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), Level2Message.class, Level2Message::writeToBuffer, b -> new Level2Message().readFromBuffer(b), Level2Message::onReceiveInternal);
+        INSTANCE.registerMessage(id.getAndIncrement(), RecipeSyncMessage.class, RecipeSyncMessage::writeToBuffer, IMessage.decode(RecipeSyncMessage::new), RecipeSyncMessage::onReceiveInternal);
     }
 
     public static void sendToClient(IMessage<?> message, World world) {
