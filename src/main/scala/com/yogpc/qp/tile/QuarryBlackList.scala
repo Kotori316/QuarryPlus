2aa97c5db4c921c76b6c228f73be95d66e727bdd
Kotori316 <Kotori316@gmail.com>
2020-03-19 14:50:39 +0900

    Added black list of ores.

--- src/main/scala/com/yogpc/qp/tile/QuarryBlackList.scala
+++ src/main/scala/com/yogpc/qp/tile/QuarryBlackList.scala
@@ -7,9 +7,12 @@ import java.util.Collections
 import com.google.gson._
 import com.yogpc.qp.QuarryPlus
 import net.minecraft.block.state.IBlockState
+import net.minecraft.init.Blocks
+import net.minecraft.item.ItemStack
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.{JsonUtils, ResourceLocation}
 import net.minecraft.world.World
+import net.minecraftforge.oredict.OreDictionary
 
 object QuarryBlackList {
   private[this] final lazy val entries: Set[Entry] = fromJson(Paths.get("config", QuarryPlus.modID, "blacklist.json"))
@@ -38,6 +41,7 @@ object QuarryBlackList {
   private[this] final val ID_AIR = QuarryPlus.modID + ":blacklist_air"
   private[this] final val ID_NAME = QuarryPlus.modID + ":blacklist_name"
   private[this] final val ID_MOD = QuarryPlus.modID + ":blacklist_modid"
+  private[this] final val ID_ORES = QuarryPlus.modID + ":blacklist_ores"
 
   object Entry extends AnyRef with JsonSerializer[Entry] with JsonDeserializer[Entry] {
     override def serialize(src: Entry, typeOfSrc: Type, context: JsonSerializationContext): JsonElement = {
@@ -60,13 +64,13 @@ object QuarryBlackList {
         id match {
           case ID_NAME => Name(new ResourceLocation(JsonUtils.getString(obj, "name")))
           case ID_MOD => Mod(JsonUtils.getString(obj, "modID"))
+          case ID_ORES => Ores
           case _ => Air
         }
       }
       idOpt.getOrElse(Air)
     }
   }
-
 
   private object Air extends Entry(ID_AIR) {
     override def test(state: IBlockState, world: World, pos: BlockPos): Boolean = world.isAirBlock(pos)
@@ -88,6 +92,44 @@ object QuarryBlackList {
     }
 
     override def toString = "BlackList for all blocks of " + modID
+  }
+
+  private object Ores extends Entry(ID_ORES) {
+    private[this] var cacheNoOre = Set(Blocks.AIR.getDefaultState)
+    private[this] var cacheOre = Set(
+      Blocks.COAL_ORE.getDefaultState,
+      Blocks.DIAMOND_ORE.getDefaultState,
+      Blocks.EMERALD_ORE.getDefaultState,
+      Blocks.GOLD_ORE.getDefaultState,
+      Blocks.IRON_ORE.getDefaultState,
+      Blocks.LAPIS_ORE.getDefaultState,
+      Blocks.LIT_REDSTONE_ORE.getDefaultState,
+      Blocks.QUARTZ_ORE.getDefaultState,
+      Blocks.REDSTONE_ORE.getDefaultState
+    )
+
+    override def test(state: IBlockState, world: World, pos: BlockPos): Boolean = {
+      if (cacheNoOre(state)) false
+      else if (cacheOre(state)) true
+      else {
+        val meta = state.getBlock.getMetaFromState(state)
+        val stack = new ItemStack(state.getBlock, 1, meta)
+        if (stack.isEmpty) {
+          cacheNoOre += state
+          false
+        } else {
+          val oreDictionaryName = OreDictionary.getOreIDs(stack)
+            .map(OreDictionary.getOreName)
+          if (oreDictionaryName.exists(_.startsWith("ore"))) {
+            cacheOre += state
+            true
+          } else {
+            cacheNoOre += state
+            false
+          }
+        }
+      }
+    }
   }
 
 }
