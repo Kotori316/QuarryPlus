51257afc04eadd1d33fba37dea1684c8dade550d
Kotori316 <Kotori316@gmail.com>
2020-04-21 08:42:52 +0900

    Fixed quarry enchantment was ignored when pump worked.

--- src/main/scala/com/yogpc/qp/machines/modules/ItemPumpModule.java
+++ src/main/scala/com/yogpc/qp/machines/modules/ItemPumpModule.java
@@ -4,6 +4,7 @@ import java.util.function.Function;
 
 import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.machines.base.APowerTile;
+import com.yogpc.qp.machines.base.EnchantmentHolder;
 import com.yogpc.qp.machines.base.HasStorage;
 import com.yogpc.qp.machines.base.IDisabled;
 import com.yogpc.qp.machines.base.IModule;
@@ -31,6 +32,18 @@ public class ItemPumpModule extends Item implements IDisabled, IModuleItem {
 
     @Override
     public <T extends APowerTile & HasStorage & ContainerQuarryModule.HasModuleInventory> Function<T, IModule> getModule(ItemStack stack) {
-        return t -> PumpModule.fromModule(t, () -> EnchantmentHelper.getEnchantmentLevel(Enchantments.UNBREAKING, stack));
+        return t -> PumpModule.fromModule(t, () -> getUnbreakingLevel(t, stack));
+    }
+
+    private static int getUnbreakingLevel(APowerTile t, ItemStack stack) {
+        int stackLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.UNBREAKING, stack);
+        int tile;
+        if (t instanceof EnchantmentHolder.EnchantmentProvider) {
+            EnchantmentHolder.EnchantmentProvider p = (EnchantmentHolder.EnchantmentProvider) t;
+            tile = p.getEnchantmentHolder().unbreaking();
+        } else {
+            tile = 0;
+        }
+        return Math.max(stackLevel, tile);
     }
 }
