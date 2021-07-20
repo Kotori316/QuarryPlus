6194ff621750dd3dd77bed6fdd72f6dc4754a1b4
Kotori316 <Kotori316@gmail.com>
2021-07-21 03:49:10 +0900

    Add enchanted Quarry to creative tab.

--- src/main/scala/com/yogpc/qp/machines/quarry/ItemQuarry.java
+++ src/main/scala/com/yogpc/qp/machines/quarry/ItemQuarry.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -37,6 +40,28 @@ class ItemQuarry extends BlockItem {
         }
         if (tag.contains("digMinY")) {
             tooltip.add(new LiteralText("DigMinY " + tag.getInt("digMinY")));
+        }
+    }
+
+    @Override
+    public void appendStacks(ItemGroup group, DefaultedList<ItemStack> stacks) {
+        super.appendStacks(group, stacks);
+        if (this.isIn(group)) {
+            var stack = new ItemStack(this);
+            {
+                var copy = stack.copy();
+                copy.addEnchantment(Enchantments.EFFICIENCY, 5);
+                copy.addEnchantment(Enchantments.UNBREAKING, 3);
+                copy.addEnchantment(Enchantments.FORTUNE, 3);
+                stacks.add(copy);
+            }
+            {
+                var copy = stack.copy();
+                copy.addEnchantment(Enchantments.EFFICIENCY, 5);
+                copy.addEnchantment(Enchantments.UNBREAKING, 3);
+                copy.addEnchantment(Enchantments.SILK_TOUCH, 1);
+                stacks.add(copy);
+            }
         }
     }
 }
