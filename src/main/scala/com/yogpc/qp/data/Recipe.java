3c945f177f4b5a5b67849c660e6e4aee6130aa55
Kotori316 <Kotori316@gmail.com>
2021-09-12 00:05:34 +0900

    Extracted common part.

--- src/main/scala/com/yogpc/qp/data/Recipe.java
+++ src/main/scala/com/yogpc/qp/data/Recipe.java
@@ -27,12 +27,15 @@ import com.yogpc.qp.machines.workbench.IngredientRecipe;
 import com.yogpc.qp.machines.workbench.IngredientWithCount;
 import com.yogpc.qp.machines.workbench.QuarryDebugCondition;
 import com.yogpc.qp.machines.workbench.WorkbenchRecipe;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.Tag;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.PotionUtils;
@@ -41,6 +44,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.Tags;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -64,103 +68,112 @@ public class Recipe extends QuarryPlusDataProvider.QuarryDataProvider {
         // Quarry Plus
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(QuarryBlock.NAME), new ItemStack(Holder.BLOCK_QUARRY), 320000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.GEMS_DIAMOND), 32)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 32)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_IRON), 64)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 16)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.ENDER_PEARLS), 4))
+            makeList(Tags.Items.GEMS_DIAMOND, 32),
+            makeList(Tags.Items.INGOTS_GOLD, 32),
+            makeList(Tags.Items.INGOTS_IRON, 64),
+            makeList(Tags.Items.DUSTS_REDSTONE, 16),
+            makeList(Tags.Items.ENDER_PEARLS, 4)
         )))).addCondition(new EnableCondition(QuarryBlock.NAME)));
         // Pump Plus
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(PumpPlusBlock.NAME), new ItemStack(Holder.BLOCK_PUMP), 320000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 16)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_IRON), 48)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 64)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.GLASS_COLORLESS), 512)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.CACTUS), 80))
+            makeList(Tags.Items.INGOTS_GOLD, 16),
+            makeList(Tags.Items.INGOTS_IRON, 48),
+            makeList(Tags.Items.DUSTS_REDSTONE, 64),
+            makeList(Tags.Items.GLASS_COLORLESS, 512),
+            makeList(Items.CACTUS, 80)
         )))).addCondition(new EnableCondition(PumpPlusBlock.NAME)));
         // Adv Pump
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(BlockAdvPump.NAME), new ItemStack(Holder.BLOCK_ADV_PUMP), 3200000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(List.of(new IngredientWithCount(Ingredient.of(Holder.BLOCK_PUMP), 2), new IngredientWithCount(Ingredient.of(Holder.ITEM_PUMP_MODULE), 2))),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Holder.BLOCK_MINING_WELL), 2)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Holder.BLOCK_MARKER), 3))
+            as(Pair.of(Ingredient.of(Holder.BLOCK_PUMP), 2), Pair.of(Ingredient.of(Holder.ITEM_PUMP_MODULE), 2)),
+            makeList(Holder.BLOCK_MINING_WELL, 2),
+            makeList(Holder.BLOCK_MARKER, 3)
         )))).addCondition(new EnableCondition(BlockAdvPump.NAME)));
         // Marker Plus
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(BlockMarker.NAME), new ItemStack(Holder.BLOCK_MARKER), 20000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 7)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_IRON), 8)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 12)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_GLOWSTONE), 4)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.LAPIS_LAZULI), 12))
+            makeList(Tags.Items.INGOTS_GOLD, 7),
+            makeList(Tags.Items.INGOTS_IRON, 8),
+            makeList(Tags.Items.DUSTS_REDSTONE, 12),
+            makeList(Tags.Items.DUSTS_GLOWSTONE, 4),
+            makeList(Items.LAPIS_LAZULI, 12)
         )))).addCondition(new EnableCondition(BlockMarker.NAME)));
         // Mining Well Plus
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(MiningWellBlock.NAME), new ItemStack(Holder.BLOCK_MINING_WELL), 160000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 3)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_IRON), 16)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 8))
+            makeList(Tags.Items.INGOTS_GOLD, 3),
+            makeList(Tags.Items.INGOTS_IRON, 16),
+            makeList(Tags.Items.DUSTS_REDSTONE, 8)
         )))).addCondition(new EnableCondition(MiningWellBlock.NAME)));
         // Status Checker
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(ItemChecker.NAME), new ItemStack(Holder.ITEM_CHECKER), 80000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 16)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_IRON), 24)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 32)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.OBSIDIAN), 4)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.LAPIS_LAZULI), 8))
+            makeList(Tags.Items.INGOTS_GOLD, 16),
+            makeList(Tags.Items.INGOTS_IRON, 24),
+            makeList(Tags.Items.DUSTS_REDSTONE, 32),
+            makeList(Tags.Items.OBSIDIAN, 4),
+            makeList(Items.LAPIS_LAZULI, 8)
         )))).addCondition(new EnableCondition(ItemChecker.NAME)));
         // Y Setter
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(YSetterItem.NAME), new ItemStack(Holder.ITEM_Y_SETTER), 80000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 32)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.GEMS_QUARTZ), 64)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.REPEATER), 16)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.COMPARATOR), 8))
+            makeList(Tags.Items.INGOTS_GOLD, 32),
+            makeList(Tags.Items.GEMS_QUARTZ, 64),
+            makeList(Items.REPEATER, 16),
+            makeList(Items.COMPARATOR, 8)
         )))).addCondition(new EnableCondition(YSetterItem.NAME)));
         // Enchantment Mover
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(BlockMover.NAME), new ItemStack(Holder.BLOCK_MOVER), 320000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.GEMS_DIAMOND), 32)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 8)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_IRON), 8)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 48)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.OBSIDIAN), 64)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.ANVIL), 2)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.ENDER_PEARLS), 2))
+            makeList(Tags.Items.GEMS_DIAMOND, 32),
+            makeList(Tags.Items.INGOTS_GOLD, 8),
+            makeList(Tags.Items.INGOTS_IRON, 8),
+            makeList(Tags.Items.DUSTS_REDSTONE, 48),
+            makeList(Tags.Items.OBSIDIAN, 64),
+            makeList(Items.ANVIL, 2),
+            makeList(Tags.Items.ENDER_PEARLS, 2)
         )))).addCondition(new EnableCondition(BlockMover.NAME)));
         // Remove Bedrock Module
         var diamond_pickaxe = new ItemStack(Items.DIAMOND_PICKAXE);
         diamond_pickaxe.removeTagKey(ItemStack.TAG_DAMAGE);
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(BedrockModuleItem.NAME), new ItemStack(Holder.ITEM_BEDROCK_MODULE), 640000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.OBSIDIAN), 32)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.STORAGE_BLOCKS_DIAMOND), 16)),
+            makeList(Tags.Items.OBSIDIAN, 32),
+            makeList(Tags.Items.STORAGE_BLOCKS_DIAMOND, 16),
             new IngredientList(new IngredientWithCount(new EnchantmentIngredient(diamond_pickaxe, List.of(new EnchantmentInstance(Enchantments.SILK_TOUCH, 1)), false), 1))
         )))).addCondition(new EnableCondition(BedrockModuleItem.NAME)));
         // Fuel Module
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location("fuel_module_normal"), new ItemStack(Holder.ITEM_FUEL_MODULE_NORMAL), 3200 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 16)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.RODS_BLAZE), 5)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.NETHERRACK), 64)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.FURNACE), 3))
+            makeList(Tags.Items.INGOTS_GOLD, 16),
+            makeList(Tags.Items.RODS_BLAZE, 5),
+            makeList(Tags.Items.NETHERRACK, 64),
+            makeList(Items.FURNACE, 3)
         )))).addCondition(new EnableCondition("fuel_module_normal")));
         // Exp Pump
         var waterBottle = PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.WATER);
         list.add(RecipeSerializeHelper.by(new FinishedWorkbenchRecipe(new IngredientRecipe(
             QuarryPlusDataProvider.location(ExpPumpBlock.NAME), new ItemStack(Holder.BLOCK_EXP_PUMP), 320000 * PowerTile.ONE_FE, true, List.of(
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_GOLD), 16)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.INGOTS_IRON), 40)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 64)),
-            new IngredientList(List.of(new IngredientWithCount(IngredientWithCount.createNbtIngredient(waterBottle), 128),
-                new IngredientWithCount(Ingredient.of(Items.EXPERIENCE_BOTTLE), 1))),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Items.HAY_BLOCK), 32)),
-            new IngredientList(new IngredientWithCount(Ingredient.of(Tags.Items.ENDER_PEARLS), 2))
+            makeList(Tags.Items.INGOTS_GOLD, 16),
+            makeList(Tags.Items.INGOTS_IRON, 40),
+            makeList(Tags.Items.DUSTS_REDSTONE, 64),
+            as(Pair.of(IngredientWithCount.createNbtIngredient(waterBottle), 128), Pair.of(Ingredient.of(Items.EXPERIENCE_BOTTLE), 1)),
+            makeList(Items.HAY_BLOCK, 32),
+            makeList(Tags.Items.ENDER_PEARLS, 2)
         )))).addCondition(new EnableCondition(ExpPumpBlock.NAME)));
 
         return list;
+    }
+
+    @Nonnull
+    private IngredientList makeList(ItemLike item, int count) {
+        return new IngredientList(new IngredientWithCount(Ingredient.of(item), count));
+    }
+
+    @Nonnull
+    private static IngredientList makeList(Tag<Item> tag, int count) {
+        return new IngredientList(new IngredientWithCount(Ingredient.of(tag), count));
     }
 
     private static final String MODULE_RECIPE_GROUP = "quarryplus:group_module";
