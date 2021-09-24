f8f1dc239ebc36a8b3a815078c5d4d385e016832
Kotori316 <Kotori316@gmail.com>
2021-09-24 11:05:45 +0900

    Fixed #134
    Moved try-catch part to get ClassNotFound

--- src/main/scala/com/yogpc/qp/integration/EnergyIntegration.java
+++ src/main/scala/com/yogpc/qp/integration/EnergyIntegration.java
@@ -4,6 +4,7 @@ import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.machines.PowerTile;
 import dev.technici4n.fasttransferlib.api.energy.EnergyApi;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import team.reborn.energy.api.EnergyStorage;
 
 @SuppressWarnings("SpellCheckingInspection")
@@ -13,14 +14,25 @@ public class EnergyIntegration {
     public static void register() {
         if (FabricLoader.getInstance().isModLoaded("team_reborn_energy")) {
             QuarryPlus.LOGGER.debug("Trying to register Reborn Energy Handler.");
-            if (RebornEnergyRegister.register()) {
-                registered = true;
+            try {
+                if (RebornEnergyRegister.register()) {
+                    registered = true;
+                }
+            } catch (Throwable error) {
+                var rebornMods = FabricLoader.getInstance().getAllMods().stream()
+                    .map(ModContainer::getMetadata).filter(s -> s.getId().contains("reborn"))
+                    .map(m -> String.format("%s@%s", m.getId(), m.getVersion().getFriendlyString())).toList();
+                QuarryPlus.LOGGER.error("QuarryPlus caught energy integration error. RebornMods: " + rebornMods, error);
             }
         }
         if (FabricLoader.getInstance().isModLoaded("fasttransferlib")) {
             QuarryPlus.LOGGER.debug("Trying to register FastTransferLib Handler.");
-            if (FastTransferLibRegister.register()) {
-                registered = true;
+            try {
+                if (FastTransferLibRegister.register()) {
+                    registered = true;
+                }
+            } catch (Throwable error) {
+                QuarryPlus.LOGGER.error("QuarryPlus caught energy integration error.", error);
             }
         }
     }
@@ -32,32 +44,22 @@ public class EnergyIntegration {
 
 class RebornEnergyRegister {
     static boolean register() {
-        try {
-            EnergyStorage.SIDED.registerForBlockEntities((blockEntity, context) -> {
-                    if (blockEntity instanceof PowerTile powerTile) return new RebornEnergyStorage(powerTile);
-                    else return null;
-                },
-                QuarryPlus.ModObjects.QUARRY_TYPE);
-            return true;
-        } catch (Throwable error) {
-            QuarryPlus.LOGGER.error("QuarryPlus caught energy integration error.", error);
-            return false;
-        }
+        EnergyStorage.SIDED.registerForBlockEntities((blockEntity, context) -> {
+                if (blockEntity instanceof PowerTile powerTile) return new RebornEnergyStorage(powerTile);
+                else return null;
+            },
+            QuarryPlus.ModObjects.QUARRY_TYPE);
+        return true;
     }
 }
 
 class FastTransferLibRegister {
     static boolean register() {
-        try {
-            EnergyApi.SIDED.registerForBlockEntities((blockEntity, context) -> {
-                    if (blockEntity instanceof PowerTile powerTile) return new FastTransferLibEnergyIO(powerTile, context);
-                    else return null;
-                },
-                QuarryPlus.ModObjects.QUARRY_TYPE);
-            return true;
-        } catch (Throwable error) {
-            QuarryPlus.LOGGER.error("QuarryPlus caught energy integration error.", error);
-            return false;
-        }
+        EnergyApi.SIDED.registerForBlockEntities((blockEntity, context) -> {
+                if (blockEntity instanceof PowerTile powerTile) return new FastTransferLibEnergyIO(powerTile, context);
+                else return null;
+            },
+            QuarryPlus.ModObjects.QUARRY_TYPE);
+        return true;
     }
 }
