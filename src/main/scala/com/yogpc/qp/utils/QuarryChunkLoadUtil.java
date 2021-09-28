8282bb3957da69ea4e78bca806d7217451660c34
Kotori316 <Kotori316@gmail.com>
2021-09-28 20:17:05 +0900

    Fixed incorrect pos was force loaded.

--- src/main/scala/com/yogpc/qp/utils/QuarryChunkLoadUtil.java
+++ src/main/scala/com/yogpc/qp/utils/QuarryChunkLoadUtil.java
@@ -5,15 +5,19 @@ import net.minecraft.core.SectionPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class QuarryChunkLoadUtil {
+    private static final Logger LOGGER = LogManager.getLogger(QuarryChunkLoadUtil.class);
+
     public static boolean isChunkLoaded(Level world, BlockPos pos) {
         return isChunkLoaded(world, pos.getX(), pos.getZ());
     }
 
     public static boolean isChunkLoaded(Level world, int x, int z) {
         if (world instanceof ServerLevel serverWorld) {
-            var key = new ChunkPos(SectionPos.sectionToBlockCoord(x), SectionPos.sectionToBlockCoord(z)).toLong();
+            var key = new ChunkPos(SectionPos.blockToSectionCoord(x), SectionPos.blockToSectionCoord(z)).toLong();
             return serverWorld.getForcedChunks().contains(key);
         } else {
             return false;
@@ -26,10 +30,11 @@ public class QuarryChunkLoadUtil {
     public static boolean makeChunkLoaded(Level world, BlockPos pos, boolean machineEnabled) {
         if (!machineEnabled) return false;
         if (world instanceof ServerLevel serverWorld) {
+            LOGGER.debug("Asked to force loading chunk at {}", pos);
             if (isChunkLoaded(world, pos)) {
                 return true;
             } else {
-                serverWorld.setChunkForced(SectionPos.sectionToBlockCoord(pos.getX()), SectionPos.sectionToBlockCoord(pos.getZ()), true);
+                serverWorld.setChunkForced(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()), true);
                 return false;
             }
         } else {
@@ -38,8 +43,9 @@ public class QuarryChunkLoadUtil {
     }
 
     public static void makeChunkUnloaded(Level world, BlockPos pos, boolean preLoaded) {
+        LOGGER.debug("Asked to unload chunk. preLoaded={}", preLoaded);
         if (!preLoaded && world instanceof ServerLevel serverWorld) {
-            serverWorld.setChunkForced(SectionPos.sectionToBlockCoord(pos.getX()), SectionPos.sectionToBlockCoord(pos.getZ()), false);
+            serverWorld.setChunkForced(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()), false);
         }
     }
 }
