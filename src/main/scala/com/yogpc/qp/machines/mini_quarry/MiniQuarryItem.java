7a46b17dd6aa9c8e2c3dc0bbf349a6b400c64ff8
Kotori316 <Kotori316@gmail.com>
2021-10-09 00:09:53 +0900

    Enchanted block in creative tab

--- src/main/scala/com/yogpc/qp/machines/mini_quarry/MiniQuarryItem.java
+++ src/main/scala/com/yogpc/qp/machines/mini_quarry/MiniQuarryItem.java
@@ -5,12 +5,26 @@ import java.util.Set;
 import com.yogpc.qp.Holder;
 import com.yogpc.qp.machines.EnchantableItem;
 import com.yogpc.qp.machines.QPBlock;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 
 public final class MiniQuarryItem extends QPBlock.QPBlockItem implements EnchantableItem {
     public MiniQuarryItem(QPBlock block) {
         super(block, new Properties().tab(Holder.TAB));
+    }
+
+    @Override
+    public void fillItemCategory(CreativeModeTab tab, NonNullList<ItemStack> stacks) {
+        super.fillItemCategory(tab, stacks);
+        if (this.allowdedIn(tab)) {
+            var stack = new ItemStack(this);
+            stack.enchant(Enchantments.BLOCK_EFFICIENCY, 5);
+            stack.enchant(Enchantments.UNBREAKING, 3);
+            stacks.add(stack);
+        }
     }
 
     @Override
