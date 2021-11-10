a71c8a73c2f7c98a1318dd126c095dd7333381fe
Kotori316 <Kotori316@gmail.com>
2021-11-10 10:19:23 +0900

    Refactoring

--- src/main/scala/com/yogpc/qp/machines/advquarry/AdvQuarryMenu.java
+++ src/main/scala/com/yogpc/qp/machines/advquarry/AdvQuarryMenu.java
@@ -1,20 +1,23 @@
 package com.yogpc.qp.machines.advquarry;
 
+import java.util.Objects;
+
 import com.yogpc.qp.QuarryPlus;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public class AdvQuarryMenu extends AbstractContainerMenu {
     public static final String GUI_ID = QuarryPlus.modID + ":gui_" + BlockAdvQuarry.NAME;
+    @NotNull
     final TileAdvQuarry quarry;
 
     public AdvQuarryMenu(int id, Player player, BlockPos pos) {
         super(QuarryPlus.ModObjects.ADV_QUARRY_MENU_TYPE, id);
-        quarry = (TileAdvQuarry) player.level.getBlockEntity(pos);
-        assert quarry != null;
+        quarry = Objects.requireNonNull((TileAdvQuarry) player.level.getBlockEntity(pos));
         for (int k = 0; k < 3; ++k) {
             for (int i1 = 0; i1 < 9; ++i1) {
                 this.addSlot(new Slot(player.getInventory(), i1 + k * 9 + 9, 8 + i1 * 18, 84 + k * 18));
