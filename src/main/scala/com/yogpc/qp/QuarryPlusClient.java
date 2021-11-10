b292a760e61ab3f5c58c6b05ef0f652cac50c37f
Kotori316 <Kotori316@gmail.com>
2021-11-10 12:17:39 +0900

    Added renderer of AdvQuarry

--- src/main/scala/com/yogpc/qp/QuarryPlusClient.java
+++ src/main/scala/com/yogpc/qp/QuarryPlusClient.java
@@ -7,6 +7,7 @@ import com.yogpc.qp.machines.misc.YSetterScreen;
 import com.yogpc.qp.machines.placer.PlacerScreen;
 import com.yogpc.qp.packet.PacketHandler;
 import com.yogpc.qp.render.Render16Marker;
+import com.yogpc.qp.render.RenderAdvQuarry;
 import com.yogpc.qp.render.RenderFlexMarker;
 import com.yogpc.qp.render.RenderMarker;
 import com.yogpc.qp.render.RenderQuarry;
@@ -30,6 +31,7 @@ public class QuarryPlusClient implements ClientModInitializer {
         BlockEntityRendererRegistry.register(QuarryPlus.ModObjects.QUARRY_TYPE, RenderQuarry::new);
         BlockEntityRendererRegistry.register(QuarryPlus.ModObjects.FLEX_MARKER_TYPE, RenderFlexMarker::new);
         BlockEntityRendererRegistry.register(QuarryPlus.ModObjects.MARKER_16_TYPE, Render16Marker::new);
+        BlockEntityRendererRegistry.register(QuarryPlus.ModObjects.ADV_QUARRY_TYPE, RenderAdvQuarry::new);
 
         ScreenRegistry.register(QuarryPlus.ModObjects.Y_SETTER_HANDLER_TYPE, YSetterScreen::new);
         ScreenRegistry.register(QuarryPlus.ModObjects.FLEX_MARKER_HANDLER_TYPE, ScreenFlexMarker::new);
