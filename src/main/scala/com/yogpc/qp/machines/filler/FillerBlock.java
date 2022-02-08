04309790c11c980b2cf943267dd3fe987b762990
Kotori316 <Kotori316@gmail.com>
2022-02-08 20:58:23 +0900

    Set drop and recipe.

--- src/main/scala/com/yogpc/qp/machines/filler/FillerBlock.java
+++ src/main/scala/com/yogpc/qp/machines/filler/FillerBlock.java
@@ -4,10 +4,14 @@ import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.machines.PowerTile;
 import com.yogpc.qp.machines.QPBlock;
 import com.yogpc.qp.utils.CombinedBlockEntityTicker;
+import com.yogpc.qp.utils.QuarryChunkLoadUtil;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -55,6 +59,30 @@ public final class FillerBlock extends QPBlock implements EntityBlock {
             return InteractionResult.sidedSuccess(level.isClientSide);
         }
         return super.use(state, level, pos, player, hand, hit);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity entity, ItemStack stack) {
+        super.setPlacedBy(level, pos, state, entity, stack);
+        if (!level.isClientSide) {
+            level.getBlockEntity(pos, QuarryPlus.ModObjects.FILLER_TYPE)
+                .ifPresent(t -> {
+                    var preForced = QuarryChunkLoadUtil.makeChunkLoaded(level, pos);
+                    t.setChunkPreLoaded(preForced);
+                });
+        }
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean moved) {
+        if (!state.is(newState.getBlock())) {
+            if (level.getBlockEntity(pos) instanceof FillerEntity filler) {
+                Containers.dropContents(level, pos, filler.container);
+                level.updateNeighbourForOutputSignal(pos, this);
+            }
+            super.onRemove(state, level, pos, newState, moved);
+        }
     }
 
     @Nullable
