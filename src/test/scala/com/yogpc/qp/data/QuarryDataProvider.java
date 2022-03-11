557fd90b4fcdc1ff023454d2d9776f9a8f44facd
Kotori316 <Kotori316@gmail.com>
2022-03-11 11:54:16 +0900

    Clean up merge

--- src/test/scala/com/yogpc/qp/data/QuarryDataProvider.java
+++ src/test/scala/com/yogpc/qp/data/QuarryDataProvider.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.util.List;
 
 import com.google.gson.GsonBuilder;
+import com.yogpc.qp.QuarryPlus;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.HashCache;
+import org.apache.logging.log4j.Logger;
 
 abstract class QuarryDataProvider implements DataProvider {
+    private static final Logger LOGGER = QuarryPlus.getLogger(QuarryPlusDataProvider.class);
     protected final DataGenerator generatorIn;
 
     protected QuarryDataProvider(DataGenerator generatorIn) {
@@ -20,8 +23,16 @@ abstract class QuarryDataProvider implements DataProvider {
         var path = generatorIn.getOutputFolder();
         var gson = new GsonBuilder().setPrettyPrinting().create();
         for (DataBuilder builder : data()) {
-            var out = path.resolve("data/%s/%s/%s.json".formatted(builder.location().getNamespace(), directory(), builder.location().getPath()));
-            DataProvider.save(gson, cache, builder.build(), out);
+            var out = path.resolve("data/%s/%s/%s.json".formatted(builder.location().getNamespace(), directory(), builder.location().getPath()))
+                .normalize()
+                .toAbsolutePath();
+            try {
+                LOGGER.info("Generating {}", out);
+                var json = builder.build();
+                DataProvider.save(gson, cache, json, out);
+            } catch (Exception e) {
+                LOGGER.error("Failed to generate json for {}", builder);
+            }
         }
     }
 
