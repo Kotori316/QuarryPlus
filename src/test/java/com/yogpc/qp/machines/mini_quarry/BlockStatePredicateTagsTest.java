42b5e1639e14d4de1bb05870d7eb7b7547d0b0f0
Kotori316 <Kotori316@gmail.com>
2022-03-25 23:28:29 +0900

    Add predicate test.

---
+++ src/test/java/com/yogpc/qp/machines/mini_quarry/BlockStatePredicateTagsTest.java
@@ -0,0 +1,140 @@
+package com.yogpc.qp.machines.mini_quarry;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.yogpc.qp.QuarryPlus;
+import com.yogpc.qp.gametest.TestUtil;
+import net.minecraft.core.BlockPos;
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.gametest.framework.GameTestGenerator;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.gametest.framework.TestFunction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.gametest.GameTestHolder;
+import net.minecraftforge.gametest.PrefixGameTestTemplate;
+import org.apache.commons.lang3.tuple.Pair;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Similar to {@link BlockStatePredicateTest}, but this is for tags and vanilla predicates,
+ * which requires actual minecraft instance.
+ */
+@GameTestHolder(QuarryPlus.modID)
+@PrefixGameTestTemplate(value = false)
+public final class BlockStatePredicateTagsTest {
+    static void cycle(BlockStatePredicate predicate) {
+        var tag = predicate.toTag();
+        var fromTag = BlockStatePredicate.fromTag(tag);
+        assertEquals(predicate, fromTag, "Expect Tag: %s, Actual Tag: %s".formatted(tag, fromTag.toTag()));
+    }
+
+    @GameTestGenerator
+    public List<TestFunction> cycleConstant() {
+        return Stream.of(BlockStatePredicate.air(), BlockStatePredicate.all(), BlockStatePredicate.fluid())
+            .map(p -> TestUtil.create(p.toString(), g -> cycle(p)))
+            .toList();
+    }
+
+    @GameTestGenerator
+    public List<TestFunction> cycleName() {
+        var names = Stream.of(Blocks.STONE, Blocks.DIAMOND_ORE).map(Block::getRegistryName);
+        var abnormalNames = Stream.of("as", "a:t", "", "fe:").map(ResourceLocation::new);
+        return Stream.concat(names, abnormalNames)
+            .map(BlockStatePredicate::name)
+            .map(p -> TestUtil.create(p.toString(), g -> cycle(p)))
+            .toList();
+    }
+
+    @GameTestGenerator
+    public List<TestFunction> cycleTag() {
+        var names = Stream.of(Tags.Blocks.STONE, Tags.Blocks.COBBLESTONE, BlockTags.ACACIA_LOGS, BlockTags.BEACON_BASE_BLOCKS)
+            .map(TagKey::location);
+        return names.map(BlockStatePredicate::tag)
+            .map(p -> TestUtil.create(p.toString(), g -> cycle(p)))
+            .toList();
+    }
+
+    static void tagTest(TagKey<Block> tag, BlockState state, GameTestHelper helper) {
+        var pos = TestUtil.getBasePos(helper).above();
+        var p = BlockStatePredicate.tag(tag.location());
+        helper.setBlock(pos, state);
+        assertTrue(p.test(state, helper.getLevel(), helper.absolutePos(pos)), "Tag: %s, State: %s".formatted(tag, state));
+    }
+
+    @GameTestGenerator
+    public List<TestFunction> tags() {
+        var tests = Stream.of(
+            Pair.of(Tags.Blocks.STONE, Blocks.STONE.defaultBlockState()),
+            Pair.of(Tags.Blocks.STONE, Blocks.ANDESITE.defaultBlockState()),
+            Pair.of(Tags.Blocks.GLASS, Blocks.GLASS.defaultBlockState()),
+            Pair.of(BlockTags.DIRT, Blocks.GRASS_BLOCK.defaultBlockState())
+        );
+        return tests
+            .map(e -> TestUtil.create("TagTest: " + e.getKey().location(), g -> tagTest(e.getKey(), e.getValue(), g)))
+            .toList();
+    }
+
+    @GameTest(template = TestUtil.EMPTY_STRUCTURE)
+    public void stoneTagFailTest(GameTestHelper helper) {
+        var tag = Tags.Blocks.STONE;
+        var p = BlockStatePredicate.tag(tag.location());
+        assertFalse(p.test(Blocks.GLASS.defaultBlockState(), helper.getLevel(), BlockPos.ZERO));
+        assertFalse(p.test(Blocks.AIR.defaultBlockState(), helper.getLevel(), BlockPos.ZERO));
+        assertFalse(p.test(Blocks.WATER.defaultBlockState(), helper.getLevel(), BlockPos.ZERO));
+        helper.succeed();
+    }
+
+    static void vanillaPredicateTest(String predicate, BlockState state, GameTestHelper helper, boolean isTrue) {
+        var pos = TestUtil.getBasePos(helper).above();
+        var p = BlockStatePredicate.predicateString(predicate);
+
+        // Prepare
+        helper.setBlock(pos, state);
+        var result = p.test(state, helper.getLevel(), helper.absolutePos(pos));
+        var message = "Predicate: %s, State %s".formatted(predicate, state);
+        if (isTrue) {
+            assertTrue(result, message);
+        } else {
+            assertFalse(result, message);
+        }
+    }
+
+    @GameTestGenerator
+    public List<TestFunction> vanillaPredicateTrue() {
+        var tests = Stream.of(
+            Pair.of("#forge:stone", Blocks.STONE.defaultBlockState()),
+            Pair.of("minecraft:stone", Blocks.STONE.defaultBlockState()),
+            Pair.of("#forge:stone", Blocks.ANDESITE.defaultBlockState()),
+            Pair.of("#forge:glass", Blocks.GLASS.defaultBlockState()),
+            Pair.of("minecraft:glass", Blocks.GLASS.defaultBlockState()),
+            Pair.of("#minecraft:dirt", Blocks.GRASS_BLOCK.defaultBlockState())
+        );
+        return tests
+            .map(e -> TestUtil.create("Vanilla(true): " + e.getKey(), g -> vanillaPredicateTest(e.getKey(), e.getValue(), g, true)))
+            .toList();
+    }
+
+    @GameTestGenerator
+    public List<TestFunction> vanillaPredicateFalse() {
+        var tests = Stream.of(
+            Pair.of("#forge:stone", Blocks.GLASS.defaultBlockState()),
+            Pair.of("minecraft:stone", Blocks.ANDESITE.defaultBlockState()),
+            Pair.of("#forge:stone", Blocks.GRASS_BLOCK.defaultBlockState()),
+            Pair.of("minecraft:glass", Blocks.AIR.defaultBlockState()),
+            Pair.of("#minecraft:dirt", Blocks.GLASS.defaultBlockState())
+        );
+        return tests
+            .map(e -> TestUtil.create("Vanilla(false): " + e.getKey(), g -> vanillaPredicateTest(e.getKey(), e.getValue(), g, false)))
+            .toList();
+    }
+}
--- src/test/java/com/yogpc/qp/machines/mover/SlotMoverTest.java
+++ src/test/java/com/yogpc/qp/machines/mover/SlotMoverTest.java
@@ -1,23 +1,24 @@
-package com.yogpc.qp.gametest;
+package com.yogpc.qp.machines.mover;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import com.yogpc.qp.QuarryPlus;
+import com.yogpc.qp.gametest.TestUtil;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestGenerator;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.gametest.framework.TestFunction;
+import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.gametest.PrefixGameTestTemplate;
 
-import static com.yogpc.qp.machines.mover.MoverAccessor.createMoverSlot;
-import static com.yogpc.qp.machines.mover.MoverAccessor.diamondTools;
-import static com.yogpc.qp.machines.mover.MoverAccessor.ironTools;
-import static com.yogpc.qp.machines.mover.MoverAccessor.netheriteTools;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -77,4 +78,19 @@ public final class SlotMoverTest {
         helper.succeed();
     }
 
+    public static Slot createMoverSlot(Container container, int index, int x, int y) {
+        return new SlotMover(container, index, x, y);
+    }
+
+    public static Stream<Item> diamondTools() {
+        return Stream.of(Items.DIAMOND_PICKAXE, Items.DIAMOND_AXE, Items.DIAMOND_SHOVEL, Items.DIAMOND_HOE, Items.DIAMOND_CHESTPLATE, Items.DIAMOND_SWORD);
+    }
+
+    public static Stream<Item> netheriteTools() {
+        return Stream.of(Items.NETHERITE_PICKAXE, Items.NETHERITE_AXE, Items.NETHERITE_SHOVEL, Items.NETHERITE_HOE, Items.NETHERITE_CHESTPLATE, Items.NETHERITE_SWORD);
+    }
+
+    public static Stream<Item> ironTools() {
+        return Stream.of(Items.IRON_PICKAXE, Items.IRON_AXE, Items.IRON_SHOVEL, Items.IRON_HOE, Items.IRON_CHESTPLATE, Items.IRON_SWORD);
+    }
 }
