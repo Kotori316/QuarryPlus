6d4a47902e691b4c5ce8599473a81e1a0fb502cc
Kotori316 <Kotori316@gmail.com>
2022-04-20 14:18:49 +0900

    Test of place block.

--- src/test/java/com/yogpc/qp/machines/placer/RemotePlacerGameTest.java
+++ src/test/java/com/yogpc/qp/machines/placer/RemotePlacerGameTest.java
@@ -12,6 +12,7 @@ import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestGenerator;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.gametest.framework.TestFunction;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 
@@ -44,14 +45,36 @@ public final class RemotePlacerGameTest {
     private void breakBlock(GameTestHelper helper, BlockPos offset) {
         var placerPos = GameTestUtil.getBasePos(helper).above();
         var targetPos = GameTestUtil.getBasePos(helper).offset(offset);
+        helper.setBlock(placerPos, Holder.BLOCK_REMOTE_PLACER);
         var tile = Objects.requireNonNull((RemotePlacerTile) helper.getBlockEntity(placerPos));
         helper.startSequence()
-            .thenExecuteAfter(1, () -> helper.setBlock(placerPos, Holder.BLOCK_REMOTE_PLACER))
             .thenExecuteAfter(1, () -> helper.setBlock(targetPos, Blocks.STONE))
             .thenExecuteAfter(1, () -> tile.targetPos = targetPos)
             .thenExecuteAfter(1, tile::breakBlock)
             .thenExecuteAfter(1, () -> helper.assertBlockNotPresent(Blocks.STONE, targetPos))
             .thenExecute(() -> assertTrue(tile.countItem(Items.STONE) > 0))
+            .thenSucceed();
+    }
+
+    @GameTestGenerator
+    public List<TestFunction> placeBlock() {
+        return StreamSupport.stream(BlockPos.randomBetweenClosed(new Random(), 4, 1, 2, 3, 4, 5, 6).spliterator(), false)
+            .map(p -> GameTestUtil.create(QuarryPlus.modID, BATCH, "PlaceBlock(%s)".formatted(p), g -> placeBlock(g, p)))
+            .toList();
+    }
+
+    void placeBlock(GameTestHelper helper, BlockPos offset) {
+        var placerPos = GameTestUtil.getBasePos(helper).above();
+        var targetPos = GameTestUtil.getBasePos(helper).offset(offset);
+        helper.setBlock(placerPos, Holder.BLOCK_REMOTE_PLACER);
+        var tile = Objects.requireNonNull((RemotePlacerTile) helper.getBlockEntity(placerPos));
+        helper.startSequence()
+            .thenExecuteAfter(1, () -> tile.targetPos = targetPos)
+            .thenExecuteAfter(1, () -> tile.setItem(0, new ItemStack(Blocks.STONE)))
+            .thenExecute(() -> assertEquals(1, tile.countItem(Items.STONE)))
+            .thenExecuteAfter(1, tile::placeBlock)
+            .thenExecuteAfter(1, () -> helper.assertBlockPresent(Blocks.STONE, targetPos))
+            .thenExecute(() -> assertEquals(0, tile.countItem(Items.STONE)))
             .thenSucceed();
     }
 }
