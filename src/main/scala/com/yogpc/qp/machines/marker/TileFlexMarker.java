460088864223b876356a987ba5e27bee946bf7ab
Kotori316 <Kotori316@gmail.com>
2022-06-04 17:39:23 +0900

    Set limit to 0. Close #191

--- src/main/scala/com/yogpc/qp/machines/marker/TileFlexMarker.java
+++ src/main/scala/com/yogpc/qp/machines/marker/TileFlexMarker.java
@@ -61,16 +61,16 @@ public class TileFlexMarker extends BlockEntity implements QuarryMarker, Checker
             int d = getDistance(max, offset, facing.getAxis());
             if (d > maxRange) {
                 max = getLimited(max, offset, facing, maxRange);
-            } else if (d < 1) {
-                max = getLimited(max, offset, facing, 1);
+            } else if (d < 0) {
+                max = getLimited(max, offset, facing, 0);
             }
         } else {
             min = min.relative(facing, amount);
             int d = getDistance(offset, min, facing.getAxis());
             if (d > maxRange) {
                 min = getLimited(min, offset, facing, maxRange);
-            } else if (d < 1) {
-                min = getLimited(min, offset, facing, 1);
+            } else if (d < 0) {
+                min = getLimited(min, offset, facing, 0);
             }
             if (facing == Direction.DOWN && min.getY() < level.getMinBuildHeight()) {
                 min = new BlockPos(min.getX(), level.getMinBuildHeight(), min.getZ());
