b53f3aa95fb0dcc4a76f10f995195d45b49a779f
Kotori316 <Kotori316@gmail.com>
2022-06-12 13:47:03 +0900

    Fix test

--- src/test/java/com/yogpc/qp/data/TagGenerator.java
+++ src/test/java/com/yogpc/qp/data/TagGenerator.java
@@ -2,17 +2,17 @@ package com.yogpc.qp.data;
 
 import com.yogpc.qp.QuarryPlus;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.Registry;
+import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.world.item.Item;
 
-final class TagGenerator extends FabricTagProvider<Item> {
+final class TagGenerator extends TagsProvider<Item> {
     TagGenerator(FabricDataGenerator dataGenerator) {
         super(dataGenerator, Registry.ITEM);
     }
 
     @Override
-    protected void generateTags() {
+    protected void addTags() {
         tag(QuarryPlus.ModObjects.TAG_MARKERS)
             .add(QuarryPlus.ModObjects.BLOCK_MARKER.blockItem)
             .add(QuarryPlus.ModObjects.BLOCK_FLEX_MARKER.blockItem)
