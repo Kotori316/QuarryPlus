cc223005e04a0a7b4672d0c8c51e5ed5804d3fdd
Kotori316 <Kotori316@gmail.com>
2023-01-05 20:52:01 +0900

    A bit of change in format.

--- src/main/scala/com/yogpc/qp/machines/TraceQuarryWork.java
+++ src/main/scala/com/yogpc/qp/machines/TraceQuarryWork.java
@@ -28,7 +28,7 @@ public final class TraceQuarryWork {
             CONTEXT = Configurator.initialize("quarryplus-config", null,
                 URI.create(Objects.requireNonNull(TraceQuarryWork.class.getResource("/quarry-log4j2.xml")).toString())
             );
-            LOGGER = CONTEXT.getLogger("TraceQuarryWork");
+            LOGGER = CONTEXT.getLogger("TQW");
             MARKER = MarkerManager.getMarker("QUARRY_TRACE");
         } else {
             CONTEXT = null;
