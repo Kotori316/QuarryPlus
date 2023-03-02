38bb1db98fc4ed04c7f4847474052c88b163b00c
Kotori316 <Kotori316@gmail.com>
2023-03-02 23:18:12 +0900

    Add check box, improving vanilla one

---
+++ src/main/scala/com/yogpc/qp/machines/misc/SmallCheckBox.java
@@ -0,0 +1,56 @@
+package com.yogpc.qp.machines.misc;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+
+public final class SmallCheckBox extends Button {
+    private static final ResourceLocation TEXTURE = new ResourceLocation("textures/gui/checkbox.png");
+    private final int checkBoxWidth;
+    private final int checkBoxHeight;
+    private boolean selected;
+
+    public SmallCheckBox(int xPos, int yPos, int totalWidth, int totalHeight, int checkBoxWidth, int checkBoxHeight, Component displayString, boolean selected, Button.OnPress onPress) {
+        super(xPos, yPos, totalWidth, totalHeight, displayString, onPress);
+        this.checkBoxWidth = checkBoxWidth;
+        this.checkBoxHeight = checkBoxHeight;
+        this.selected = selected;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    @Override
+    public void onPress() {
+        this.selected = !this.selected;
+        super.onPress();
+    }
+
+    @Override
+    public void renderButton(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
+        Minecraft minecraft = Minecraft.getInstance();
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        RenderSystem.enableDepthTest();
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, this.alpha);
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        Font font = minecraft.font;
+        float uOffset = this.isHoveredOrFocused() ? 20.0F : 0.0F;
+        float vOffset = this.isSelected() ? 20.0F : 0.0F;
+        blit(pPoseStack, this.x, this.y + this.height / 2 - this.checkBoxHeight / 2,
+            this.checkBoxWidth, this.checkBoxHeight, uOffset, vOffset, 20, 20, 64, 64);
+        this.renderBg(pPoseStack, minecraft, pMouseX, pMouseY);
+        int color = 0x404040;
+        int labelOffset = this.checkBoxWidth / 5;
+        font.draw(pPoseStack, this.getMessage(), this.x + this.checkBoxWidth + labelOffset, this.y + ((float) this.height - 7) / 2,
+            color | Mth.ceil(this.alpha * 255.0F) << 24);
+    }
+}
