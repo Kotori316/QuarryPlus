042fc5e1852ce29f64e76f3631406f4f996f1a88
Kotori316 <Kotori316@gmail.com>
2023-03-24 21:48:23 +0900

    Fix build

--- src/test/java/com/yogpc/qp/AccessItemTest.java
+++ src/test/java/com/yogpc/qp/AccessItemTest.java
@@ -1,6 +1,6 @@
 package com.yogpc.qp;
 
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import org.junit.jupiter.api.Assertions;
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 final class AccessItemTest extends QuarryPlusTest {
     @Test
     void testItems() {
-        ResourceLocation id = Registry.ITEM.getKey(Items.DIAMOND);
+        ResourceLocation id = BuiltInRegistries.ITEM.getKey(Items.DIAMOND);
         Assertions.assertEquals("minecraft:diamond", id.toString());
     }
 }
