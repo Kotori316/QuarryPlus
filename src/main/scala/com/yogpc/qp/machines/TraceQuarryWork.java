bb414d2532733b113f8a5b52be9914fe4a7ee5c3
Kotori316 <Kotori316@gmail.com>
2023-10-29 14:52:25 +0900

    Create log file always

--- src/main/scala/com/yogpc/qp/machines/TraceQuarryWork.java
+++ src/main/scala/com/yogpc/qp/machines/TraceQuarryWork.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
@@ -16,6 +17,8 @@ import org.apache.logging.log4j.core.config.Configurator;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.URI;
+import java.security.SecureClassLoader;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -27,96 +30,81 @@ public final class TraceQuarryWork {
     private static final LoggerContext CONTEXT;
     private static final Logger LOGGER;
     private static final Marker MARKER;
+    private static final Marker WARNING_MARKER;
+
+    private static class DummyLoader extends SecureClassLoader {
+    }
 
     static {
         enabled = QuarryPlus.config.common.logAllQuarryWork.get();
-        if (enabled) {
-            CONTEXT = Configurator.initialize("quarryplus-config", null,
-                URI.create(Objects.requireNonNull(TraceQuarryWork.class.getResource("/quarry-log4j2.xml")).toString())
-            );
-            LOGGER = CONTEXT.getLogger("TQW");
-            MARKER = MarkerManager.getMarker("QUARRY_TRACE");
-        } else {
-            CONTEXT = null;
-            LOGGER = null;
-            MARKER = null;
+        CONTEXT = Configurator.initialize("quarryplus-config", new DummyLoader(),
+            URI.create(Objects.requireNonNull(TraceQuarryWork.class.getResource("/quarry-log4j2.xml")).toString())
+        );
+        // temporal variable to set level, as org.apache.logging.log4j.Logger doesn't provide setters.
+        var l = CONTEXT.getLogger("TQW");
+        LOGGER = l;
+        MARKER = MarkerManager.getMarker("QUARRY_TRACE");
+        WARNING_MARKER = MarkerManager.getMarker("QUARRY_WARNING");
+        if (!enabled) {
+            l.setLevel(Level.WARN);
         }
     }
 
     public static void startWork(PowerTile tile, BlockPos pos, int energyInMachine) {
-        if (enabled)
-            LOGGER.info(MARKER, "{} Started work with {} FE", header(tile, pos), energyInMachine);
+        LOGGER.info(MARKER, "{} Started work with {} FE", header(tile, pos), energyInMachine);
     }
 
     public static void changeTarget(PowerTile tile, BlockPos pos, String state) {
-        if (enabled)
-            LOGGER.debug(MARKER, "{} Target changed in {}", header(tile, pos), state);
+        LOGGER.debug(MARKER, "{} Target changed in {}", header(tile, pos), state);
     }
 
     public static void progress(PowerTile tile, BlockPos pos, BlockPos targetPos, String reason) {
-        if (enabled)
-            LOGGER.debug(MARKER, "{} ({},{},{}) {}", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), reason);
+        LOGGER.debug(MARKER, "{} ({},{},{}) {}", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), reason);
     }
 
     public static void canBreakCheck(PowerTile tile, BlockPos pos, BlockPos targetPos, BlockState state, String detail) {
-        if (enabled) {
-            LOGGER.info(MARKER, "{} ({},{},{}) {} State({})", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), detail, state);
-        }
+        LOGGER.info(MARKER, "{} ({},{},{}) {} State({})", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), detail, state);
     }
 
     public static void blockRemoveFailed(PowerTile tile, BlockPos pos, BlockPos targetPos, BlockState state, BreakResult breakResult) {
-        if (enabled)
-            LOGGER.info(MARKER, "{} ({},{},{}) {} {}", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), breakResult, state);
+        LOGGER.info(MARKER, "{} ({},{},{}) {} {}", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), breakResult, state);
     }
 
     public static void blockRemoveSucceed(PowerTile tile, BlockPos pos, BlockPos targetPos, BlockState state, List<ItemStack> drops, int exp, long consumedEnergy) {
-        if (enabled) {
-            LOGGER.debug(MARKER, "{} ({},{},{}) SUCCESS({} FE) {} EXP={} ({})", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), consumedEnergy / PowerTile.ONE_FE, state, exp,
-                drops.stream().map(s -> "%dx %s".formatted(s.getCount(), ForgeRegistries.ITEMS.getKey(s.getItem()))).collect(Collectors.joining(",")));
-        }
+        LOGGER.debug(MARKER, "{} ({},{},{}) SUCCESS({} FE) {} EXP={} ({})", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), consumedEnergy / PowerTile.ONE_FE, state, exp,
+            drops.stream().map(s -> "%dx %s".formatted(s.getCount(), ForgeRegistries.ITEMS.getKey(s.getItem()))).collect(Collectors.joining(",")));
     }
 
     public static void blockRemoveSucceed(PowerTile tile, BlockPos pos, BlockPos targetPos, List<BlockState> state, Map<ItemKey, Long> drops, int exp, long consumedEnergy) {
-        if (enabled) {
-            var stateCount = state.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-            LOGGER.debug(MARKER, "{} ({},{},{}) SUCCESS({} FE) {} EXP={} ({})", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), consumedEnergy / PowerTile.ONE_FE, stateCount, exp,
-                drops.entrySet().stream().map(s -> "%dx %s".formatted(s.getValue(), s.getKey().getId())).collect(Collectors.joining(",")));
-        }
+        var stateCount = state.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        LOGGER.debug(MARKER, "{} ({},{},{}) SUCCESS({} FE) {} EXP={} ({})", header(tile, pos), targetPos.getX(), targetPos.getY(), targetPos.getZ(), consumedEnergy / PowerTile.ONE_FE, stateCount, exp,
+            drops.entrySet().stream().map(s -> "%dx %s".formatted(s.getValue(), s.getKey().getId())).collect(Collectors.joining(",")));
     }
 
     public static void unexpected(PowerTile tile, BlockPos pos, String reason) {
-        if (enabled)
-            LOGGER.warn(MARKER, "{} {}", header(tile, pos), reason);
+        LOGGER.warn(MARKER, "{} {}", header(tile, pos), reason);
     }
 
     public static void finishWork(PowerTile tile, BlockPos pos, int energyInMachine) {
-        if (enabled)
-            LOGGER.info(MARKER, "{} Finished work with {} FE", header(tile, pos), energyInMachine);
+        LOGGER.info(MARKER, "{} Finished work with {} FE", header(tile, pos), energyInMachine);
     }
 
     public static void convertItem(ItemKey before, ItemKey after) {
-        if (enabled)
-            LOGGER.debug(MARKER, "Convert {} to {}", before, after);
+        LOGGER.debug(MARKER, "Convert {} to {}", before, after);
     }
 
     public static void transferItem(@Nullable BlockEntity from, @Nullable IItemHandler dest, ItemKey itemKey, int count) {
-        if (enabled) {
-            String header = from != null ? header(from, from.getBlockPos()) : "ItemHandler Extraction";
-            LOGGER.debug(MARKER, "{} Transfer {}x {} to {}", header, count, itemKey, dest);
-        }
+        String header = from != null ? header(from, from.getBlockPos()) : "ItemHandler Extraction";
+        LOGGER.debug(MARKER, "{} Transfer {}x {} to {}", header, count, itemKey, dest);
     }
 
     public static void transferFluid(@Nullable BlockEntity from, @Nullable IFluidHandler dest, FluidKey fluidKey, int amount) {
-        if (enabled) {
-            String header = from != null ? header(from, from.getBlockPos()) : "FluidHandler Extraction";
-            LOGGER.debug(MARKER, "{} Transfer {}mB of {} to {}", header, amount, fluidKey, dest);
-        }
+        String header = from != null ? header(from, from.getBlockPos()) : "FluidHandler Extraction";
+        LOGGER.debug(MARKER, "{} Transfer {}mB of {} to {}", header, amount, fluidKey, dest);
     }
 
     public static void noDrops(BlockState state, BlockPos pos, ItemStack tool) {
-        if (enabled) {
-            LOGGER.debug("{} at ({},{},{}) has no drops with {}", state, pos.getX(), pos.getY(), pos.getZ(), tool);
-        }
+        LOGGER.debug(MARKER, "{} at ({},{},{}) has no drops with {}", state, pos.getX(), pos.getY(), pos.getZ(), tool);
     }
 
     private static String header(BlockEntity tile, BlockPos pos) {
