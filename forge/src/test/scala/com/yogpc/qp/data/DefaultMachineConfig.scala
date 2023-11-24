2d7a58119d87616d8d26348226cb1d8f84c8bd15
Kotori316 <Kotori316@gmail.com>
2023-11-24 20:48:32 +0900

    Fix migration error in data generation

--- forge/src/test/scala/com/yogpc/qp/data/DefaultMachineConfig.scala
+++ forge/src/test/scala/com/yogpc/qp/data/DefaultMachineConfig.scala
@@ -1,17 +1,16 @@
 package com.yogpc.qp.data
 
-import java.util.Collections
-
 import com.google.gson.JsonObject
 import com.yogpc.qp.{Holder, QuarryPlus}
 import net.minecraft.data.DataGenerator
 import net.minecraft.resources.ResourceLocation
 
+import java.util.Collections
 import scala.jdk.javaapi.CollectionConverters
 
 class DefaultMachineConfig(generator: DataGenerator)
   extends QuarryDataProvider(generator) {
-  override def directory(): String = "forge/src" // To save to root dir
+  override def directory(): String = "../.." // To save to root dir
 
   override def data(): java.util.List[_ <: DataBuilder] = {
     Collections.singletonList(DefaultMachineConfig)
