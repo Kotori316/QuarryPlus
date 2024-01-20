82e7700b8657fd7965251c885132d72d69c35db3
Kotori316 <Kotori316@gmail.com>
2024-01-20 14:25:28 +0900

    Change y setter signature

--- src/main/scala/com/yogpc/qp/machines/misc/YSetterItem.java
+++ src/main/scala/com/yogpc/qp/machines/misc/YSetterItem.java
@@ -3,8 +3,8 @@ package com.yogpc.qp.machines.misc;
 import com.yogpc.qp.Holder;
 import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.machines.QPItem;
+import com.yogpc.qp.packet.ClientSyncMessage;
 import com.yogpc.qp.packet.PacketHandler;
-import com.yogpc.qp.packet.TileMessage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -32,10 +32,11 @@ public class YSetterItem extends QPItem {
         var level = context.getLevel();
         var player = context.getPlayer();
         var tile = level.getBlockEntity(context.getClickedPos());
-        if (YAccessor.get(tile) != null) {
+        YAccessor<?> accessor = YAccessor.get(tile);
+        if (accessor != null) {
             if (!level.isClientSide) {
                 if (player instanceof ServerPlayer p) {
-                    PacketHandler.sendToClientPlayer(new TileMessage(tile), p);
+                    PacketHandler.sendToClientPlayer(new ClientSyncMessage(accessor.getEntity()), p);
                     NetworkHooks.openScreen(p, new YSetterScreenHandler(tile.getBlockPos(), tile.getBlockState().getBlock()), context.getClickedPos());
                 }
 
