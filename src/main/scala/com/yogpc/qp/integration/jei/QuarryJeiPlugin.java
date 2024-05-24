1e160f19073a22afcf9a35eb051dbcd2e91cfae5
Kotori316 <Kotori316@gmail.com>
2024-05-24 23:54:32 +0900

    Not to load JEI plugin when REI is installed

--- src/main/scala/com/yogpc/qp/integration/jei/QuarryJeiPlugin.java
+++ src/main/scala/com/yogpc/qp/integration/jei/QuarryJeiPlugin.java
@@ -9,6 +9,7 @@ import com.yogpc.qp.QuarryPlus;
 import com.yogpc.qp.machines.workbench.RecipeFinder;
 import com.yogpc.qp.machines.workbench.ScreenWorkbench;
 import com.yogpc.qp.machines.workbench.WorkbenchRecipe;
+import me.shedaniel.rei.plugincompatibilities.api.REIPluginCompatIgnore;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
@@ -21,6 +22,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
 @JeiPlugin
+@REIPluginCompatIgnore
 public class QuarryJeiPlugin implements IModPlugin {
     static IJeiRuntime jeiRuntime;
     static WorkBenchRecipeCategory workBenchRecipeCategory;
