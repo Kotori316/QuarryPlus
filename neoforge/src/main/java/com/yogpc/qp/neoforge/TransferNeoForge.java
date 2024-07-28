67e5a8aa36fdd0ea469324973fcf525444b41d95
Kotori316 <Kotori316@gmail.com>
2024-07-28 15:31:02 +0900

    Transfer items in NeoForge

--- neoforge/src/main/java/com/yogpc/qp/neoforge/TransferNeoForge.java
+++ neoforge/src/main/java/com/yogpc/qp/neoforge/TransferNeoForge.java
@@ -5,10 +5,15 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.items.ItemHandlerHelper;
 
 public final class TransferNeoForge implements PlatformAccess.Transfer {
     @Override
     public ItemStack transferItem(Level level, BlockPos pos, ItemStack stack, Direction side, boolean simulate) {
-        return stack;
+        var handler = level.getCapability(Capabilities.ItemHandler.BLOCK, pos, side);
+        if (handler == null) return stack;
+
+        return ItemHandlerHelper.insertItem(handler, stack, simulate);
     }
 }
