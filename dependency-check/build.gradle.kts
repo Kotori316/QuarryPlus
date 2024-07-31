5867c8a88d2f6df391bd1332405db4aba7b27c13
Kotori316 <Kotori316@gmail.com>
2024-07-31 21:53:22 +0900

    Remove unused

--- dependency-check/build.gradle.kts
+++ dependency-check/build.gradle.kts
@@ -3,27 +3,11 @@ plugins {
 }
 
 dependencies {
-    // JEI
-    /*implementation(
-        group = "mezz.jei",
-        name = "jei-1.21-forge",
-        version = project.property("jei_forge_version").toString()
-    )
-    implementation(
-        group = "mezz.jei",
-        name = "jei-1.21-fabric",
-        version = project.property("jei_fabric_version").toString()
-    )
-    implementation(
-        group = "mezz.jei",
-        name = "jei-1.21-neoforge",
-        version = project.property("jei_neoforge_version").toString()
-    )*/
     // Parchment
     implementation(
         group = "org.parchmentmc.data",
         name = "parchment-${project.property("parchment.minecraft")}",
         version = project.property("parchment.mapping").toString(),
-        ext = "zip"
+        ext = "zip",
     )
 }
