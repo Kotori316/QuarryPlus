ae8818e095e95fd184e725e2213692d31e67af4a
Kotori316 <Kotori316@gmail.com>
2024-08-04 15:50:43 +0900

    Forge GameTest

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -8,6 +8,27 @@ plugins {
 
 val modId = "QuarryPlus".lowercase()
 
+sourceSets {
+    val mainSourceSet by main
+    val gameTestSourceSet by gameTest
+    create("runGame") {
+        val sourceSet = this
+        project.configurations {
+            named(sourceSet.compileClasspathConfigurationName) {
+                extendsFrom(
+                    project.configurations.named(mainSourceSet.compileClasspathConfigurationName).get(),
+                    project.configurations.named(gameTestSourceSet.compileClasspathConfigurationName).get(),
+                )
+            }
+            named(sourceSet.runtimeClasspathConfigurationName) {
+                extendsFrom(
+                    project.configurations.named(mainSourceSet.runtimeClasspathConfigurationName).get(),
+                )
+            }
+        }
+    }
+}
+
 minecraft {
     mappings(
         mapOf(
@@ -39,6 +60,18 @@ minecraft {
                 }
             }
         }
+
+        create("gameTestServer") {
+            property("forge.enabledGameTestNamespaces", modId)
+            workingDirectory(project.file("game-test"))
+            property("bsl.debug", "true")
+            jvmArgs("-ea")
+            mods {
+                create("main") {
+                    source(sourceSets["runGame"])
+                }
+            }
+        }
     }
 }
 
@@ -49,7 +82,13 @@ dependencies {
         isTransitive = false
     }
     runtimeOnly(libs.jei.forge)
+    implementation("com.kotori316:test-utility-forge:21.0-SNAPSHOT") {
+        isTransitive = false
+    }
     implementation("net.sf.jopt-simple:jopt-simple:5.0.4") { version { strictly("5.0.4") } }
+
+    "runGameRuntimeOnly"(platform(libs.junit))
+    "runGameRuntimeOnly"(libs.jupiter)
 }
 
 // Share with common
@@ -63,6 +102,39 @@ tasks.processResources {
 }
 
 // Forge stuff
+
+tasks.named("compileRunGameJava", JavaCompile::class) {
+    project.findProject(":common")?.let {
+        source(it.sourceSets.main.get().java)
+        source(it.sourceSets.gameTest.get().java)
+    }
+    source(project.sourceSets.main.get().java)
+    source(project.sourceSets.gameTest.get().java)
+}
+tasks.named("processRunGameResources", ProcessResources::class) {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    project.findProject(":common")?.let {
+        from(it.sourceSets.main.get().resources)
+        from(it.sourceSets.gameTest.get().resources)
+    }
+    from(project.sourceSets.main.get().resources)
+    from(project.sourceSets.gameTest.get().resources)
+
+    val projectVersion = project.version.toString()
+    val minecraft = "1.21"
+    inputs.property("version", projectVersion)
+    inputs.property("minecraftVersion", minecraft)
+    listOf("fabric.mod.json", "META-INF/mods.toml", "META-INF/neoforge.mods.toml").forEach { fileName ->
+        filesMatching(fileName) {
+            expand(
+                "version" to projectVersion,
+                "update_url" to "https://version.kotori316.com/get-version/${minecraft}/${project.name}/${modId}",
+                "mc_version" to minecraft,
+            )
+        }
+    }
+}
+
 sourceSets.forEach {
     val dir = layout.buildDirectory.dir("forgeSourcesSets/${it.name}")
     it.output.setResourcesDir(dir)
