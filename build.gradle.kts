00bdbfe3a0518dd9988fe4c9a9a2d5e8bffe18ab
Kotori316 <Kotori316@gmail.com>
2024-08-29 00:24:39 +0900

    Add GitHub release integration without files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,39 @@
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     alias(libs.plugins.idea.ext)
+    alias(libs.plugins.github.release)
+}
+
+val changelog: Provider<String> = provider {
+    val time = ZonedDateTime.now(ZoneId.of("Asia/Tokyo")).withNano(0).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+    val fromFile = project.file(project.property("changelog_file")!!).readText()
+    val shortFormat = fromFile.split(Regex("^# ", RegexOption.MULTILINE), limit = 3)[1]
+    """
+        QuarryPlus for Minecraft ${libs.versions.minecraft.get()}
+        
+        Released at $time
+        
+        | Name | Version |
+        | -- | -- |
+        | Minecraft | ${libs.versions.minecraft.get()} |
+        | Forge | ${libs.versions.forge.get()} |
+        | Fabric | ${libs.versions.fabric.api.get()} |
+        | NeoForge | ${libs.versions.neoforge.get()} |
+        
+    """.trimIndent() + System.lineSeparator() + shortFormat
+}
+
+githubRelease {
+    owner = "Kotori316"
+    repo = "QuarryPlus"
+    token(project.findProperty("githubToken") as? String ?: System.getenv("REPO_TOKEN") ?: "")
+    targetCommitish = project.property("mod.branch") as String
+    tagName = provider { "v${project.project(":common").version}" }
+    releaseName = provider { "v${project.project(":common").version} for Minecraft ${libs.versions.minecraft.get()}" }
+    prerelease = false
+    body = changelog
+    dryRun = (System.getenv("RELEASE_DEBUG") ?: "true").toBoolean()
 }
