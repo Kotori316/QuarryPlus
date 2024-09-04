b601798fb4cc3fba4424fce200ae64e527526bda
Kotori316 <Kotori316@gmail.com>
2024-09-05 00:18:42 +0900

    Add getter for machine storage

---
+++ common/src/main/java/com/yogpc/qp/machine/MachineStorageHolder.java
@@ -0,0 +1,24 @@
+package com.yogpc.qp.machine;
+
+import java.util.Optional;
+import java.util.ServiceLoader;
+
+public interface MachineStorageHolder<T> {
+    MachineStorage getMachineStorage(T instance);
+
+    Class<T> supportingClass();
+
+    @SuppressWarnings("unchecked")
+    static <T> Optional<MachineStorageHolder<T>> getHolder(T object) {
+        if (object == null) {
+            return Optional.empty();
+        }
+
+        for (MachineStorageHolder<?> holder : ServiceLoader.load(MachineStorageHolder.class)) {
+            if (holder.supportingClass().isAssignableFrom(object.getClass())) {
+                return Optional.of((MachineStorageHolder<T>) holder);
+            }
+        }
+        return Optional.empty();
+    }
+}
