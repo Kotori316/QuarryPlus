dafb49d2787409ca1a4f1501a06ecfd82b6b67c4
Kotori316 <Kotori316@gmail.com>
2024-10-10 21:32:49 +0900

    Add ChunkLoader for machines

--- common/src/main/java/com/yogpc/qp/machine/misc/QuarryChunkLoader.java
+++ common/src/main/java/com/yogpc/qp/machine/misc/QuarryChunkLoader.java
@@ -3,16 +3,22 @@ package com.yogpc.qp.machine.misc;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.yogpc.qp.QuarryPlus;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 public sealed interface QuarryChunkLoader {
+    Marker MARKER = MarkerFactory.getMarker(QuarryChunkLoader.class.getSimpleName());
     Codec<QuarryChunkLoader> CODEC = Codec.STRING.dispatch(QuarryChunkLoader::type, QuarryChunkLoader::codec);
 
     static boolean isChunkLoaded(ServerLevel level, BlockPos pos) {
         var chunkPos = new ChunkPos(pos);
-        return level.getForcedChunks().contains(chunkPos.toLong());
+        var result = level.getForcedChunks().contains(chunkPos.toLong());
+        QuarryPlus.LOGGER.info(MARKER, "Check state of chunk loading x={}, z={}, loaded={}", chunkPos.x, chunkPos.z, result);
+        return result;
     }
 
     static QuarryChunkLoader of(ServerLevel level, BlockPos pos) {
@@ -64,12 +70,14 @@ public sealed interface QuarryChunkLoader {
         public void makeChunkLoaded(ServerLevel level) {
             var chunkPos = new ChunkPos(pos);
             level.setChunkForced(chunkPos.x, chunkPos.z, true);
+            QuarryPlus.LOGGER.info(MARKER, "Force chunk load at x={}, z={}", chunkPos.x, chunkPos.z);
         }
 
         @Override
         public void makeChunkUnLoaded(ServerLevel level) {
             var chunkPos = new ChunkPos(pos);
             level.setChunkForced(chunkPos.x, chunkPos.z, false);
+            QuarryPlus.LOGGER.info(MARKER, "Remove chunk loading at x={}, z={}", chunkPos.x, chunkPos.z);
         }
 
         @Override
