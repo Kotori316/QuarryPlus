886503990f5a5be59222a32c2c150f1cb34c0b6d
Kotori316 <Kotori316@gmail.com>
2025-02-10 23:30:43 +0900

    Add ticket for mining

--- src/main/scala/com/yogpc/qp/utils/QuarryChunkLoadUtil.java
+++ src/main/scala/com/yogpc/qp/utils/QuarryChunkLoadUtil.java
@@ -1,12 +1,18 @@
 package com.yogpc.qp.utils;
 
 import com.yogpc.qp.QuarryPlus;
+import com.yogpc.qp.machines.Area;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.server.level.TicketType;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
 
 public class QuarryChunkLoadUtil {
     private static final Logger LOGGER = QuarryPlus.getLogger(QuarryChunkLoadUtil.class);
@@ -46,6 +52,37 @@ public class QuarryChunkLoadUtil {
         LOGGER.debug("Asked to unload chunk at {}. preLoaded={}", pos, preLoaded);
         if (!preLoaded && world instanceof ServerLevel serverWorld) {
             serverWorld.setChunkForced(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()), false);
+        }
+    }
+
+    static final TicketType<ChunkPos> QUARRY_PLUS_MINING = TicketType.create("%s:%s".formatted(QuarryPlus.modID, "mining_ticket"), Comparator.comparingLong(ChunkPos::toLong));
+    /**
+     * Border level
+     *
+     * @see net.minecraft.server.level.ChunkLevel
+     * @see <a href="https://minecraft.wiki/w/Chunk#Java_Edition_chunk_loading">WIKI</a>
+     */
+    static final int TICKET_LEVEL = 33;
+
+    public static void makeChunkLoadedForMining(ServerLevel level, Area area) {
+        var distanceManager = level.getChunkSource().chunkMap.getDistanceManager();
+        operateForChunks(area, pos -> distanceManager.addTicket(QUARRY_PLUS_MINING, pos, TICKET_LEVEL, pos));
+    }
+
+    public static void removeChunkLoadTicket(ServerLevel level, Area area) {
+        var distanceManager = level.getChunkSource().chunkMap.getDistanceManager();
+        operateForChunks(area, pos -> distanceManager.removeTicket(QUARRY_PLUS_MINING, pos, TICKET_LEVEL, pos));
+    }
+
+    static void operateForChunks(Area area, Consumer<ChunkPos> consumer) {
+        var minX = SectionPos.blockToSectionCoord(area.minX());
+        var minZ = SectionPos.blockToSectionCoord(area.minZ());
+        var maxX = SectionPos.blockToSectionCoord(area.maxX());
+        var maxZ = SectionPos.blockToSectionCoord(area.maxZ());
+        for (int x = minX; x <= maxX; x++) {
+            for (int z = minZ; z <= maxZ; z++) {
+                consumer.accept(new ChunkPos(x, z));
+            }
         }
     }
 }
