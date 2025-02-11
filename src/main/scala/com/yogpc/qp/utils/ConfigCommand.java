07ff8cdd8391b628ea31c2c20aa866e6f985676c
Kotori316 <Kotori316@gmail.com>
2025-02-11 17:18:17 +0900

    Add command to check chunk ticket

--- src/main/scala/com/yogpc/qp/utils/ConfigCommand.java
+++ src/main/scala/com/yogpc/qp/utils/ConfigCommand.java
@@ -18,12 +18,19 @@ import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
 import net.minecraft.commands.synchronization.ArgumentTypeInfo;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.DistanceManager;
+import net.minecraft.server.level.Ticket;
+import net.minecraft.util.SortedArraySet;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -45,7 +52,9 @@ public final class ConfigCommand {
             Commands.argument("name", new SelectorArgument(configNames))
                 .then(Commands.argument("value", BoolArgumentType.bool())
                     .executes(c -> changeMachineSetting(SelectorArgument.getString(c, "name"), BoolArgumentType.getBool(c, "value"), c)));
-        event.getDispatcher().register(parent.then(configCommand.then(setCommand)));
+        var checkTicketCommand = Commands.literal("checkTicket").executes(c -> checkTicket(c, BlockPos.containing(c.getSource().getPosition())))
+            .then(Commands.argument("pos", BlockPosArgument.blockPos()).executes(c -> checkTicket(c, BlockPosArgument.getBlockPos(c, "pos"))));
+        event.getDispatcher().register(parent.then(configCommand.then(setCommand)).then(checkTicketCommand));
     }
 
     private static int changeMachineSetting(String name, boolean enabled, CommandContext<CommandSourceStack> commandContext) throws CommandSyntaxException {
@@ -138,5 +147,26 @@ public final class ConfigCommand {
         public ArgumentTypeInfo<SelectorArgument, ?> type() {
             return INFO;
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static int checkTicket(CommandContext<CommandSourceStack> commandContext, BlockPos pos) throws CommandSyntaxException {
+        var data = commandContext.getSource();
+        var distanceManager = data.getLevel().getChunkSource().chunkMap.getDistanceManager();
+        var chunkPos = new ChunkPos(pos);
+        try {
+            var method = ObfuscationReflectionHelper.findMethod(DistanceManager.class, "m_140857_", long.class);
+            method.setAccessible(true);
+            var tickets = (SortedArraySet<Ticket<?>>) method.invoke(distanceManager, chunkPos.toLong());
+
+            QuarryPlus.LOGGER.info("Tickets for chunk {}({}) is {}", chunkPos, pos, tickets);
+            data.sendSystemMessage(Component.literal("Tickets for chunk %s(%s) is %s".formatted(chunkPos, pos, tickets)));
+        } catch (ReflectiveOperationException e) {
+            QuarryPlus.LOGGER.error("Can't access to getTickets method.", e);
+            var supplier = new SimpleCommandExceptionType(Component.literal(e.getMessage()));
+            throw supplier.create();
+        }
+
+        return Command.SINGLE_SUCCESS;
     }
 }
