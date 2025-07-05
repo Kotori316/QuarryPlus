fb8dccedd470e015a64c271cc6aaabd60bfa531c
Kotori316 <Kotori316@gmail.com>
2025-07-06 02:00:23 +0900

    Update data generator

--- neoforge/src/commonDataGen/scala/com/yogpc/qp/common/data/QuarryItemTagProvider.scala
+++ neoforge/src/commonDataGen/scala/com/yogpc/qp/common/data/QuarryItemTagProvider.scala
@@ -1,19 +1,22 @@
 package com.yogpc.qp.common.data
 
-import com.yogpc.qp.QuarryPlus
+import com.yogpc.qp.{PlatformAccess, QuarryPlus}
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
-import net.minecraft.data.tags.{ItemTagsProvider, TagsProvider}
 import net.minecraft.world.item.Items
-import net.minecraft.world.level.block.Block
+import net.neoforged.neoforge.common.data.ItemTagsProvider
 
 import java.util.concurrent.CompletableFuture
 
-class QuarryItemTagProvider(output: PackOutput, lookupProvider: CompletableFuture[HolderLookup.Provider], blockTags: CompletableFuture[TagsProvider.TagLookup[Block]])
-  extends ItemTagsProvider(output, lookupProvider, blockTags, QuarryPlus.modID) {
+class QuarryItemTagProvider(output: PackOutput, lookupProvider: CompletableFuture[HolderLookup.Provider])
+  extends ItemTagsProvider(output, lookupProvider, QuarryPlus.modID) {
 
   override def addTags(provider: HolderLookup.Provider): Unit = {
-    copy(markerBlockTag, markerItemTag)
+    val holder = PlatformAccess.getAccess.registerObjects()
+    tag(markerItemTag)
+      .add(holder.markerBlock().get().blockItem)
+      .add(holder.flexibleMarkerBlock().get().blockItem)
+      .add(holder.chunkMarkerBlock().get().blockItem)
     tag(quarryPickaxeTag).add(Items.DIAMOND_PICKAXE, Items.NETHERITE_PICKAXE)
   }
 }
