0f206f8973c6c495108f7356c1fcd8953723bf42
Kotori316 <Kotori316@gmail.com>
2026-05-04 11:54:34 +0900

    Change changelog location in the publication plugin

--- publish-all/build.gradle.kts
+++ publish-all/build.gradle.kts
@@ -64,6 +64,10 @@ fun getReleaseJarFiles(): List<Provider<RegularFile>> {
         list.add(project(":neoforge").tasks.named("sourcesJar", AbstractArchiveTask::class).flatMap { it.archiveFile })
     }
     // Dummy file for the common project, where no artifacts are available
-    list.add(project.layout.file(provider { rootProject.file("changelog/1.21.md") }))
+    list.add(project.layout.file(provider {
+        rootProject.file(
+            project.property("changelog_file") as String
+        )
+    }))
     return list
 }
