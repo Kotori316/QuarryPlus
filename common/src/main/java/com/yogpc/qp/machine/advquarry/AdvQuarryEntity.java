ec8c5f9396cec8e861b164149275c1ca7033be5b
Kotori316 <Kotori316@gmail.com>
2026-09-13 09:35:09 +0900

    Use marker

--- common/src/main/java/com/yogpc/qp/machine/advquarry/AdvQuarryEntity.java
+++ common/src/main/java/com/yogpc/qp/machine/advquarry/AdvQuarryEntity.java
@@ -455,7 +455,7 @@ public abstract class AdvQuarryEntity extends PowerEntity implements ClientSync 
         // First check event
         var eventResult = checkBreakEvent(serverLevel, player, state, target, blockEntity);
         if (eventResult.canceled()) {
-            QuarryPlus.LOGGER.debug("An BreakEvent canceled removing block({}) at {} by {}", state, target, getClass().getSimpleName());
+            QuarryPlus.LOGGER.debug(MARKER, "An BreakEvent canceled removing block({}) at {} by {}", state, target, getClass().getSimpleName());
             return WorkResult.FAIL_EVENT;
         }
         // Second, check modules
